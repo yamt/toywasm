432a930211ef0bef1741cc447ddd5146d183cc50
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-07-30 19:24:44 +0900

    wasm-on-wasm.sh: script to build and run wasm version
    ```
    === native ===
    toywasm wasm interpreter
    __clang_version__ = 13.0.1
    __x86_64__ defined
    __APPLE__ defined
    USE_SEPARATE_EXECUTE defined
    USE_TAILCALL defined
    
    real    0m0.003s
    user    0m0.001s
    sys     0m0.001s
    === wasm ===
    toywasm wasm interpreter
    __clang_version__ = 13.0.0 (https://github.com/llvm/llvm-project fd1d8c2f04dde23bee0fb3a7d069a9b1046da979)
    __wasi__ defined
    USE_SEPARATE_EXECUTE defined
    
    real    0m0.009s
    user    0m0.007s
    sys     0m0.002s
    === wasm on wasm ===
    toywasm wasm interpreter
    __clang_version__ = 13.0.0 (https://github.com/llvm/llvm-project fd1d8c2f04dde23bee0fb3a7d069a9b1046da979)
    __wasi__ defined
    USE_SEPARATE_EXECUTE defined
    
    real    0m0.183s
    user    0m0.180s
    sys     0m0.002s
    === wasm on wasm on wasm ===
    toywasm wasm interpreter
    __clang_version__ = 13.0.0 (https://github.com/llvm/llvm-project fd1d8c2f04dde23bee0fb3a7d069a9b1046da979)
    __wasi__ defined
    USE_SEPARATE_EXECUTE defined
    
    real    0m17.916s
    user    0m17.713s
    sys     0m0.067s
    ```

---
+++ wasm-on-wasm.sh
@@ -0,0 +1,75 @@
+#! /bin/sh
+
+set -e
+
+WASI_SDK_DIR=.wasi-sdk
+DIST_DIR=.dist
+
+mkdir -p ${DIST_DIR}
+
+fetch_wasi_sdk()
+{
+    UNAME=$(uname -s)
+    case ${UNAME} in
+    Darwin)
+        PLATFORM=macos
+        ;;
+    Linux)
+        PLATFORM=linux
+        ;;
+    *)
+        echo "Unknown uname ${UNAME}"
+        exit 1
+        ;;
+    esac
+    MAJOR=14
+    MINOR=0
+    TAR=wasi-sdk-${MAJOR}.${MINOR}-${PLATFORM}.tar.gz
+    if [ ! -f ${DIST_DIR}/${TAR} ]; then
+        URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${MAJOR}/${TAR}
+        curl -L -o ${DIST_DIR}/${TAR} ${URL}
+    fi
+    pax -rz \
+    -f ${DIST_DIR}/${TAR} \
+    -s"!^wasi-sdk-${MAJOR}\.${MINOR}!${WASI_SDK_DIR}!"
+}
+
+test -d "${WASI_SDK_DIR}" || fetch_wasi_sdk
+
+# see also: https://github.com/WebAssembly/tail-call
+cmake \
+-B build.wasm \
+-DWASI_SDK_PREFIX=$(pwd)/${WASI_SDK_DIR} \
+-DCMAKE_TOOLCHAIN_FILE=${WASI_SDK_DIR}/share/cmake/wasi-sdk.cmake \
+-DBUILD_TESTING=OFF \
+-DTOYWASM_USE_TAILCALL=OFF \
+.
+cmake --build build.wasm
+
+cmake \
+-B build.native \
+-DBUILD_TESTING=OFF \
+.
+cmake --build build.native
+
+echo "=== native ==="
+time \
+./build.native/toywasm --version
+
+echo "=== wasm ==="
+time \
+./build.native/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --version
+
+echo "=== wasm on wasm ==="
+time \
+./build.native/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --version
+
+echo "=== wasm on wasm on wasm ==="
+time \
+./build.native/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --wasi --wasi-dir . -- \
+./build.wasm/toywasm --version
