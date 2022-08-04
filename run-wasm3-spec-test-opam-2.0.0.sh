e94f9f43892696f3280fe00634b68fed053223f2
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-08-04 23:55:35 +0900

    run-wasm3-spec-test-opam-2.0.0.sh: use pre-converted tests
    it seems that wast2json on GHA runners is older than i expected.

--- run-wasm3-spec-test-opam-2.0.0.sh
+++ run-wasm3-spec-test-opam-2.0.0.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
-./prepare-spec-test.sh
-exec ./run-wasm3-spec-test.sh --spec-dir $(pwd -P)/.opam-2.0.0/test "$@"
+
+TEST_DIR=.opam-2.0.0
+fetch_spec_json()
+{
+    REPO=https://github.com/yamt/wasm-spec-test
+    REF=opam-2.0.0
+    mkdir "${TEST_DIR}"
+    git -C "${TEST_DIR}" init
+    git -C "${TEST_DIR}" fetch --depth 1 ${REPO} ${REF}
+    git -C "${TEST_DIR}" checkout FETCH_HEAD
+}
+test -d "${TEST_DIR}" || fetch_spec_json
+
+exec ./run-wasm3-spec-test.sh --spec-dir $(pwd -P)/${TEST_DIR}/test "$@"
