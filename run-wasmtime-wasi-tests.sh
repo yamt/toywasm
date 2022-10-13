226c016d303b6667936bbe86846ee5144092f618
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-10-13 21:04:57 +0900

    run-wasmtime-wasi-tests.sh: skip fd_readdir on 32-bit target
    to workaround qemu issue
    https://gitlab.com/qemu-project/qemu/-/issues/263
    
    also, refactor black lists

--- run-wasmtime-wasi-tests.sh
+++ run-wasmtime-wasi-tests.sh
@@ -16,6 +16,10 @@ fetch_test_bin()
 
 test -d "${WASM_DIR}" || fetch_test_bin
 
+SKIPLIST=
+BLACKLIST="${THIS_DIR}/wasmtime-wasi-tests-blacklist.txt"
+BLACKLIST="${BLACKLIST} ${THIS_DIR}/wasmtime-wasi-tests-blacklist-$(uname -s).txt"
+
 if [ "${WASM_ON_WASM:-0}" -ne 0 ]; then
     run_wasi_test()
     {
@@ -30,30 +34,41 @@ else
     {
         ${EXE} --wasi-dir ${TMP} ${w} ${TMP};
     }
+    if ${EXE} --version | grep -F "sizeof(void *) = 4"; then
+        SKIPLIST="${SKIPLIST} ${THIS_DIR}/wasmtime-wasi-tests-skip-32bit.txt"
+    fi
 fi
 
 # suppress useless messages on macOS
 # toywasm(46834,0x11068c600) malloc: nano zone abandoned due to inability to preallocate reserved vm space.
 export MallocNanoZone=0
 
-BLACKLIST="${THIS_DIR}/wasmtime-wasi-tests-blacklist-$(uname -s).txt"
 TOTAL=0
 FAIL=0
 EXPECTED_FAIL=0
 UNEXPECTED_SUCCESS=0
+SKIPPED=0
 cd "${WASM_DIR}"
 for w in *.wasm; do
+    TOTAL=$((TOTAL + 1))
+    if [ -n "${SKIPLIST}" ]; then
+        if grep "^${w%%.wasm}$" ${SKIPLIST}; then
+            echo "=== ${w} skipped"
+            SKIPPED=$((SKIPPED + 1))
+            continue
+        fi
+    fi
     echo "=== ${w}"
     TMP=$(mktemp -d)
     if run_wasi_test; then
-        if grep "^${w%%.wasm}$" "${BLACKLIST}"; then
+        if grep "^${w%%.wasm}$" ${BLACKLIST}; then
             echo "=== ${w} succeeded (unexpected)"
             UNEXPECTED_SUCCESS=$((UNEXPECTED_SUCCESS + 1))
         else
             echo "=== ${w} succeeded (expected)"
         fi
     else
-        if grep "^${w%%.wasm}$" "${BLACKLIST}"; then
+        if grep "^${w%%.wasm}$" ${BLACKLIST}; then
             echo "=== ${w} failed (expected)"
             EXPECTED_FAIL=$((EXPECTED_FAIL + 1))
         else
@@ -61,9 +76,8 @@ for w in *.wasm; do
             FAIL=$((FAIL + 1))
         fi
     fi
-    TOTAL=$((TOTAL + 1))
     rm -r ${TMP}
 done
-echo "fail / expected_fail / unexpected_success / total = ${FAIL} / ${EXPECTED_FAIL} / ${UNEXPECTED_SUCCESS} / ${TOTAL}"
+echo "fail / expected_fail / unexpected_success / skipped / total = ${FAIL} / ${EXPECTED_FAIL} / ${UNEXPECTED_SUCCESS} / ${SKIPPED} / ${TOTAL}"
 test ${UNEXPECTED_SUCCESS} -eq 0
 test ${FAIL} -eq 0
