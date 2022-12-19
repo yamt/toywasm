4ad8b282fe45b3f9169c284220834100ee982d3b
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-12-20 00:53:25 +0900

    platform.h: add several macros for compiler attributes

--- lib/platform.h
+++ lib/platform.h
@@ -13,3 +13,35 @@
 #define __predict_false(x) (!(x))
 #endif
 #endif /* !defined(__predict_true) */
+
+#if !defined(__must_check)
+#if defined(__GNUC__)
+#define __must_check __attribute__((warn_unused_result))
+#else
+#define __must_check
+#endif
+#endif
+
+#if !defined(__malloc_like)
+#if defined(__GNUC__)
+#define __malloc_like __attribute__((malloc))
+#else
+#define __malloc_like
+#endif
+#endif
+
+#if !defined(__alloc_size)
+#if defined(__GNUC__)
+#define __alloc_size(a) __attribute__((alloc_size(a)))
+#else
+#define __alloc_size(a)
+#endif
+#endif
+
+#if !defined(__alloc_size2)
+#if defined(__GNUC__)
+#define __alloc_size2(a, b) __attribute__((alloc_size(a, b)))
+#else
+#define __alloc_size2(a, b)
+#endif
+#endif
