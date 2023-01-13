12231cee8c47d002a783b7e28bbc76c7bf729a6d
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-01-13 23:01:05 +0900

    benchmark/ffmpeg.sh
    * print versions
    
    * remove an iwasm option for fast-jit as it seems no longer necessary

--- benchmark/ffmpeg.sh
+++ benchmark/ffmpeg.sh
@@ -7,7 +7,9 @@ set -e
 
 run()
 {
-    echo "===== $@"
+    echo "===== $1"
+    shift 1
+    echo "----- $@"
     OUTPUT=$(mktemp)
     ./test/run-ffmpeg.sh /usr/bin/time -l "$@" > ${OUTPUT} 2>&1
     grep -E "(real.*user.*sys|instructions retired|peak memory footprint|maximum resident set size)" ${OUTPUT}
@@ -19,16 +21,16 @@ run()
 # which use malloc+read to load wasm binaries.
 
 TOYWASM=${TOYWASM:-toywasm}
-run ${TOYWASM} --wasi --wasi-dir .video --
-# run ${TOYWASM} --wasi --wasi-dir .video --disable-jump-table --disable-localtype-cellidx --disable-resulttype-cellidx --
+run "toywasm" ${TOYWASM} --wasi --wasi-dir .video --
+# run "toywasm" ${TOYWASM} --wasi --wasi-dir .video --disable-jump-table --disable-localtype-cellidx --disable-resulttype-cellidx --
 
-run wasm3 --dir .video --
+run "$(wasm3 --version|head -1)" wasm3 --dir .video --
 
-run iwasm.fast --dir=.video
-run iwasm.classic --dir=.video
+run "$(iwasm.fast --version) (fast interpreter)" iwasm.fast --dir=.video
+run "$(iwasm.classic --version) (classic interpreter)" iwasm.classic --dir=.video
 # Note: i needed to tweak these size options manually to run
 # this particular wasm binary
-run iwasm.fast-jit --dir=.video --jit-codecache-size=100000000 --stack-size=100000
+run "$(iwasm.fast-jit --version) (fast jit)" iwasm.fast-jit --dir=.video --jit-codecache-size=100000000
 
-run wasmer run --dir .video --
-run wasmtime run --dir .video --
+run "$(wasmer --version)" wasmer run --dir .video --
+run "$(wasmtime --version)" wasmtime run --dir .video --
