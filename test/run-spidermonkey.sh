447779d87470add5f8c5439de07c40ecfe275b86
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-01-13 23:07:00 +0900

    whitespace

--- test/run-spidermonkey.sh
+++ test/run-spidermonkey.sh
@@ -4,7 +4,7 @@ set -e
 BIN=.spidermonkey/spidermonkey.wasm
 if [ ! -f ${BIN} ]; then
     URL=https://registry-cdn.wapm.io/contents/mozilla/spidermonkey/0.0.1/build/spidermonkey.wasm
-	mkdir -p .spidermonkey
+    mkdir -p .spidermonkey
     curl -L -o ${BIN} ${URL}
 fi
 OUT=$(mktemp)
