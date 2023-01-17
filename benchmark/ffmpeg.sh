2880a1005d9518889738005071bc23a908e6d280
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-01-17 20:37:21 +0900

    benchmark/ffmpeg.sh: add wasmedge

--- benchmark/ffmpeg.sh
+++ benchmark/ffmpeg.sh
@@ -33,9 +33,12 @@ run "$(wasm3 --version|head -1)" wasm3 --dir .video --
 
 run "$(iwasm.fast --version) (fast interpreter)" iwasm.fast --dir=.video
 run "$(iwasm.classic --version) (classic interpreter)" iwasm.classic --dir=.video
+
 # Note: i needed to tweak these size options manually to run
 # this particular wasm binary
 run "$(iwasm.fast-jit --version) (fast jit)" iwasm.fast-jit --dir=.video --jit-codecache-size=100000000
+
+run "$(wasmedge --version) (interpreter)" wasmedge --dir .video --
 
 run "$(wasmer --version)" wasmer run --dir .video --
 run "$(wasmtime --version)" wasmtime run --dir .video --
