1c848317e23a93a7e780fb6e586003803030cb54
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-01-19 22:33:05 +0900

    benchmark/ffmpeg.sh: separate interpreters and jit

--- benchmark/ffmpeg.sh
+++ benchmark/ffmpeg.sh
@@ -25,6 +25,8 @@ rm -rf ~/Library/Caches/BytecodeAlliance.wasmtime
 # it might be a bit unfair to compare the memory stats with engines
 # which use malloc+read to load wasm binaries.
 
+echo "+++++++++++ Interpreters +++++++++++"
+
 TOYWASM=${TOYWASM:-toywasm}
 run "toywasm" ${TOYWASM} --wasi --wasi-dir .video --
 # run "toywasm" ${TOYWASM} --wasi --wasi-dir .video --disable-jump-table --disable-localtype-cellidx --disable-resulttype-cellidx --
@@ -34,11 +36,13 @@ run "$(wasm3 --version|head -1)" wasm3 --dir .video --
 run "$(iwasm.fast --version) (fast interpreter)" iwasm.fast --dir=.video
 run "$(iwasm.classic --version) (classic interpreter)" iwasm.classic --dir=.video
 
+run "$(wasmedge --version) (interpreter)" wasmedge --dir .video --
+
+echo "+++++++++++ JIT ++++++++++++++++++++"
+
 # Note: i needed to tweak these size options manually to run
 # this particular wasm binary
 run "$(iwasm.fast-jit --version) (fast jit)" iwasm.fast-jit --dir=.video --jit-codecache-size=100000000
-
-run "$(wasmedge --version) (interpreter)" wasmedge --dir .video --
 
 run "$(wasmer --version)" wasmer run --dir .video --
 run "$(wasmtime --version)" wasmtime run --dir .video --
