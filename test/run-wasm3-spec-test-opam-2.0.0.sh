6dda54c0bf0e3a1fc4b72fa1676cf60da3134ffc
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-01-23 21:29:29 +0900

    reduce dup in test scripts

--- test/run-wasm3-spec-test-opam-2.0.0.sh
+++ test/run-wasm3-spec-test-opam-2.0.0.sh
@@ -1,15 +1,6 @@
 #! /bin/sh
 
-TEST_DIR=.wasm-spec-test
-fetch_spec_json()
-{
-    REPO=https://github.com/yamt/wasm-spec-test
-    REF=783d5b09e7ba1322b85a53071ed7f7e2c70739a2
-    mkdir "${TEST_DIR}"
-    git -C "${TEST_DIR}" init
-    git -C "${TEST_DIR}" fetch --depth 1 ${REPO} ${REF}
-    git -C "${TEST_DIR}" checkout FETCH_HEAD
-}
-test -d "${TEST_DIR}" || fetch_spec_json
+set -e
 
+./test/fetch-spec-test.sh
 exec ./test/run-wasm3-spec-test.sh --spec-dir $(pwd -P)/${TEST_DIR}/test "$@"
