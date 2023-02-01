9f4350ffd88def4a1708daca3ae8d4253e7001f9
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-02-01 22:14:59 +0900

    wasi: implement the rest of socket api

--- lib/wasi.c
+++ lib/wasi.c
@@ -3107,6 +3107,230 @@ fail:
         return host_ret;
 }
 
+static int
+wasi_sock_recv(struct exec_context *ctx, struct host_instance *hi,
+               const struct functype *ft, const struct cell *params,
+               struct cell *results)
+{
+        WASI_TRACE;
+        struct wasi_instance *wasi = (void *)hi;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t wasifd = HOST_FUNC_PARAM(ft, params, 0, i32);
+        uint32_t iov_addr = HOST_FUNC_PARAM(ft, params, 1, i32);
+        uint32_t iov_count = HOST_FUNC_PARAM(ft, params, 2, i32);
+        uint32_t riflags = HOST_FUNC_PARAM(ft, params, 3, i32);
+        uint32_t retp = HOST_FUNC_PARAM(ft, params, 4, i32);
+        uint32_t roflagsp = HOST_FUNC_PARAM(ft, params, 5, i32);
+        struct iovec *hostiov = NULL;
+        struct wasi_fdinfo *fdinfo = NULL;
+        int hostfd;
+        int host_ret = 0;
+        int ret;
+        int flags = 0;
+        if ((riflags & ~(WASI_RIFLAG_RECV_PEEK | WASI_RIFLAG_RECV_WAITALL)) !=
+            0) {
+                ret = EINVAL;
+                goto fail;
+        }
+        if ((riflags & WASI_RIFLAG_RECV_PEEK) != 0) {
+                flags |= MSG_PEEK;
+        }
+        if ((riflags & WASI_RIFLAG_RECV_WAITALL) != 0) {
+                /*
+                 * Note: it seems difficult (or impossible) to emulate
+                 * blocking MSG_WAITALL behavior with a non-blocking
+                 * underlying socket.
+                 *
+                 * Note: MSG_WAITALL behavior on a non-blocking socket
+                 * varies among platforms.
+                 * eg. BSD honors MSG_WAITALL. Linux ignores it.
+                 */
+                ret = ENOTSUP;
+                goto fail;
+        }
+        if (iov_count > INT_MAX) {
+                ret = EOVERFLOW;
+                goto fail;
+        }
+        ret = wasi_hostfd_lookup(wasi, wasifd, &hostfd, &fdinfo);
+        if (ret != 0) {
+                goto fail;
+        }
+        host_ret = wasi_copyin_iovec(ctx, iov_addr, iov_count, &hostiov, &ret);
+        if (host_ret != 0 || ret != 0) {
+                goto fail;
+        }
+        ssize_t n;
+        struct msghdr msg;
+retry:
+        memset(&msg, 0, sizeof(msg));
+        msg.msg_iov = hostiov;
+        msg.msg_iovlen = iov_count;
+#if defined(__wasi__)
+        n = -1;
+        errno = ENOSYS;
+#else
+        n = recvmsg(hostfd, &msg, flags);
+#endif
+        if (n == -1) {
+                ret = errno;
+                assert(ret > 0);
+                if (emulate_blocking(ctx, fdinfo, POLLIN, ret, &host_ret,
+                                     &ret)) {
+                        goto retry;
+                }
+                if (host_ret != 0 || ret != 0) {
+                        goto fail;
+                }
+                goto fail;
+        }
+        if (n > UINT32_MAX) {
+                ret = EOVERFLOW;
+                goto fail;
+        }
+        uint16_t roflags = 0;
+        if ((msg.msg_flags & MSG_TRUNC) != 0) {
+                roflags = WASI_ROFLAG_RECV_DATA_TRUNCATED;
+        }
+        uint32_t r = host_to_le32(n);
+        host_ret = wasi_copyout(ctx, &r, retp, sizeof(r));
+        if (host_ret != 0) {
+                goto fail;
+        }
+        uint16_t roflags_wasm = host_to_le16(roflags);
+        host_ret = wasi_copyout(ctx, &roflags_wasm, roflagsp,
+                                sizeof(roflags_wasm));
+        if (host_ret != 0) {
+                goto fail;
+        }
+fail:
+        wasi_fdinfo_release(wasi, fdinfo);
+        if (host_ret == 0) {
+                HOST_FUNC_RESULT_SET(ft, results, 0, i32,
+                                     wasi_convert_errno(ret));
+        }
+        free(hostiov);
+        return host_ret;
+}
+
+static int
+wasi_sock_send(struct exec_context *ctx, struct host_instance *hi,
+               const struct functype *ft, const struct cell *params,
+               struct cell *results)
+{
+        WASI_TRACE;
+        struct wasi_instance *wasi = (void *)hi;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t wasifd = HOST_FUNC_PARAM(ft, params, 0, i32);
+        uint32_t iov_addr = HOST_FUNC_PARAM(ft, params, 1, i32);
+        uint32_t iov_count = HOST_FUNC_PARAM(ft, params, 2, i32);
+        uint32_t siflags = HOST_FUNC_PARAM(ft, params, 3, i32);
+        uint32_t retp = HOST_FUNC_PARAM(ft, params, 4, i32);
+        struct iovec *hostiov = NULL;
+        struct wasi_fdinfo *fdinfo = NULL;
+        int hostfd;
+        int host_ret = 0;
+        int ret;
+        if (siflags != 0) {
+                ret = EINVAL;
+                goto fail;
+        }
+        if (iov_count > INT_MAX) {
+                ret = EOVERFLOW;
+                goto fail;
+        }
+        ret = wasi_hostfd_lookup(wasi, wasifd, &hostfd, &fdinfo);
+        if (ret != 0) {
+                goto fail;
+        }
+        host_ret = wasi_copyin_iovec(ctx, iov_addr, iov_count, &hostiov, &ret);
+        if (host_ret != 0 || ret != 0) {
+                goto fail;
+        }
+        ssize_t n;
+        struct msghdr msg;
+retry:
+        memset(&msg, 0, sizeof(msg));
+        msg.msg_iov = hostiov;
+        msg.msg_iovlen = iov_count;
+#if defined(__wasi__)
+        n = -1;
+        errno = ENOSYS;
+#else
+        n = sendmsg(hostfd, &msg, 0);
+#endif
+        if (n == -1) {
+                ret = errno;
+                assert(ret > 0);
+                if (emulate_blocking(ctx, fdinfo, POLLOUT, ret, &host_ret,
+                                     &ret)) {
+                        goto retry;
+                }
+                if (host_ret != 0 || ret != 0) {
+                        goto fail;
+                }
+                goto fail;
+        }
+        if (n > UINT32_MAX) {
+                ret = EOVERFLOW;
+                goto fail;
+        }
+        uint32_t r = host_to_le32(n);
+        host_ret = wasi_copyout(ctx, &r, retp, sizeof(r));
+fail:
+        wasi_fdinfo_release(wasi, fdinfo);
+        if (host_ret == 0) {
+                HOST_FUNC_RESULT_SET(ft, results, 0, i32,
+                                     wasi_convert_errno(ret));
+        }
+        free(hostiov);
+        return host_ret;
+}
+
+static int
+wasi_sock_shutdown(struct exec_context *ctx, struct host_instance *hi,
+                   const struct functype *ft, const struct cell *params,
+                   struct cell *results)
+{
+        WASI_TRACE;
+        struct wasi_instance *wasi = (void *)hi;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t wasifd = HOST_FUNC_PARAM(ft, params, 0, i32);
+        uint32_t sdflags = HOST_FUNC_PARAM(ft, params, 1, i32);
+        uint32_t retp = HOST_FUNC_PARAM(ft, params, 2, i32);
+        struct wasi_fdinfo *fdinfo = NULL;
+        int hostfd;
+        int ret;
+        int how = 0;
+        switch (sdflags) {
+        case WASI_SDFLAG_RD | WASI_SDFLAG_WR:
+                how = SHUT_RDWR;
+                break;
+        case WASI_SDFLAG_RD:
+                how = SHUT_RD;
+                break;
+        case WASI_SDFLAG_WR:
+                how = SHUT_WR;
+                break;
+        default:
+                ret = EINVAL;
+                goto fail;
+        }
+        ret = wasi_hostfd_lookup(wasi, wasifd, &hostfd, &fdinfo);
+        if (ret != 0) {
+                goto fail;
+        }
+        ret = shutdown(hostfd, how);
+        if (ret == -1) {
+                ret = errno;
+                assert(ret > 0);
+        }
+fail:
+        wasi_fdinfo_release(wasi, fdinfo);
+        HOST_FUNC_RESULT_SET(ft, results, 0, i32, wasi_convert_errno(ret));
+        return 0;
+}
+
 const struct host_func wasi_funcs[] = {
         /* args */
         WASI_HOST_FUNC(args_get, "(ii)i"),
@@ -3169,11 +3393,9 @@ const struct host_func wasi_funcs[] = {
 
         /* sock */
         WASI_HOST_FUNC(sock_accept, "(iii)i"),
-#if 0 /* TODO */
         WASI_HOST_FUNC(sock_recv, "(iiiiii)i"),
         WASI_HOST_FUNC(sock_send, "(iiiii)i"),
         WASI_HOST_FUNC(sock_shutdown, "(ii)i"),
-#endif
 };
 
 int
