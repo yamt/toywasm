3fed6e5eb2ce30780531402993d6ac0d9e9b382d
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-02-10 13:34:45 +0900

    benchmark/ffmpeg.sh: add a url

--- benchmark/ffmpeg.sh
+++ benchmark/ffmpeg.sh
@@ -45,7 +45,7 @@ run "$(wasmedge --version) (interpreter)" wasmedge --dir .video --
 #
 # run "wasm-interp $(wasm-interp --version)" wasm-interp --wasi --dir .video --
 
-# https://github.com/paritytech/wasmi
+# https://github.com/paritytech/wasmi/issues/661
 # i couldn't find how to pass wasi cli arguments
 #
 # https://github.com/paritytech/wasmi/blob/master/crates/cli/src/main.rs#L22-L23
