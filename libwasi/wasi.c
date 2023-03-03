4d594d5308829ca48858b855e9c2b9f465788af0
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-03-04 03:09:36 +0900

    proc_exit: comment

--- libwasi/wasi.c
+++ libwasi/wasi.c
@@ -714,6 +714,26 @@ wasi_proc_exit(struct exec_context *ctx, struct host_instance *hi,
         struct wasi_instance *wasi = (void *)hi;
         HOST_FUNC_CONVERT_PARAMS(ft, params);
         uint32_t code = HOST_FUNC_PARAM(ft, params, 0, i32);
+        /*
+         * Note: While our embedder API (wasi_instance_exit_code) has
+         * no problem to propagate a full 32-bit value, toywasm cli
+         * can't represent full range with its exit code, especially
+         * when it's running on a wasm runtime with restricted wasi
+         * exit code.
+         *
+         * Note: wasmtime traps for exit code >= 126.
+         *
+         * Note: in preview1, only 0 has a defined meaning.
+         * https://github.com/WebAssembly/WASI/blob/ddfe3d1dda5d1473f37ecebc552ae20ce5fd319a/legacy/preview1/witx/wasi_snapshot_preview1.witx#L460-L462
+         * > An exit code of 0 indicates successful termination of
+         * > the program. The meanings of other values is dependent on
+         * > the environment.
+         *
+         * Note: A change to restrict wasi exit code to [0,126) has been
+         * made to ephemeral. But it has never been a part of preview1.
+         * https://github.com/WebAssembly/WASI/pull/235
+         * https://github.com/WebAssembly/WASI/pull/510
+         */
         toywasm_mutex_lock(&wasi->lock);
         wasi->exit_code = code;
         toywasm_mutex_unlock(&wasi->lock);
