9bb31470cb3aa3392a02f949c2ee6302fd9de436
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-03-21 15:28:53 +0900

    bump wasm-spec-test version for exception-handling

--- test/fetch-spec-test.sh
+++ test/fetch-spec-test.sh
@@ -6,7 +6,7 @@ TEST_DIR=${TEST_DIR:-.wasm-spec-test}
 fetch_spec_json()
 {
     REPO=https://github.com/yamt/wasm-spec-test
-    REF=29ab36f9ea07bbb3e329e4a19393567a1782039e
+    REF=adf7cd944fe6658135c4cf854c2fa6a801dde05b
     mkdir "${TEST_DIR}"
     git -C "${TEST_DIR}" init
     git -C "${TEST_DIR}" fetch --depth 1 ${REPO} ${REF}
