fc8d9df6df5b8ba1596912e0c4aeb21f55f118b7
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-04-19 20:09:07 +0900

    test/pipe.py: workaround a netbsg bug
    cf. http://gnats.netbsd.org/cgi-bin/query-pr-single.pl?number=57369

--- test/pipe.py
+++ test/pipe.py
@@ -7,4 +7,8 @@ import select
 
 p = select.poll()
 p.register(sys.stdout, select.POLLHUP)
-p.poll(-1)
+# http://gnats.netbsd.org/cgi-bin/query-pr-single.pl?number=57369
+while True:
+    l = p.poll(1)
+    if l:
+        break
