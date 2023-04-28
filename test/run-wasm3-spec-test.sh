d02f6272257df93dc3b1734264d92154d3e68fcb
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-04-28 20:50:43 +0900

    run-wasm3-spec-test.sh: update my fork of wasm3
    Namely this change:
    ```
    commit 6d7abdcda765cb19d507ebcf2a1069d69d4199fc
    Author: YAMAMOTO Takashi <imuwoto@gmail.com>
    Date:   Fri Apr 28 20:43:53 2023 +0900
    
        run-spec-test.py: make tests fail on unexpected traps
    ```

--- test/run-wasm3-spec-test.sh
+++ test/run-wasm3-spec-test.sh
@@ -7,7 +7,7 @@ WASM3_DIR=.wasm3
 fetch_wasm3()
 {
     REPO=https://github.com/yamt/wasm3
-    REF=fe3af0043a168a2841d2ed1402c0a532376f0c86
+    REF=6d7abdcda765cb19d507ebcf2a1069d69d4199fc
     mkdir "${WASM3_DIR}"
     git -C "${WASM3_DIR}" init
     git -C "${WASM3_DIR}" fetch --depth 1 ${REPO} ${REF}
