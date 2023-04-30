e85c9e3d78a4cbabb8bcd1a21da4f481518f1d00
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-05-01 00:11:24 +0900

    a new file i forgot to add

---
+++ lib/load_context.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "context.h"
+#include "load_context.h"
+#include "validation.h"
+
+void
+load_context_init(struct load_context *ctx)
+{
+        memset(ctx, 0, sizeof(*ctx));
+        report_init(&ctx->report);
+        load_options_set_defaults(&ctx->options);
+}
+
+void
+load_context_clear(struct load_context *ctx)
+{
+        report_clear(&ctx->report);
+        bitmap_free(&ctx->refs);
+        validation_context_clear(ctx->vctx);
+        free(ctx->vctx);
+}
