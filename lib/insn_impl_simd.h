0f7beb0c31ec28f54d9ed94589dbb18aa860bb0d
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-05-18 06:32:31 +0900

    simd: implement v128.loadXXX_lane

--- lib/insn_impl_simd.h
+++ lib/insn_impl_simd.h
@@ -2,12 +2,37 @@
  * https://github.com/WebAssembly/simd
  */
 
+#define READ_LANEIDX(VAR, N)                                                  \
+        READ_U8(VAR);                                                         \
+        CHECK(VAR < N)
+
+/*
+ * Note: i128x1 is just for implementation convenience
+ */
+
+#define LANEPTR128(val) (&(val)->u.v128)
+#define LANEPTR64(val) (val)->u.v128.i64
+#define LANEPTR32(val) (val)->u.v128.i32
+#define LANEPTR16(val) (val)->u.v128.i16
+#define LANEPTR8(val) (val)->u.v128.i8
+
+#define READ_LANEIDX1(VAR) const uint8_t VAR = 0
+#define READ_LANEIDX2(VAR) READ_LANEIDX(VAR, 2)
+#define READ_LANEIDX4(VAR) READ_LANEIDX(VAR, 4)
+#define READ_LANEIDX8(VAR) READ_LANEIDX(VAR, 8)
+#define READ_LANEIDX16(VAR) READ_LANEIDX(VAR, 16)
+#define READ_LANEIDX32(VAR) READ_LANEIDX(VAR, 32)
+
 /*
  * MEM - num of bits in memory
- * STACK_TYPE - type on stack (eg. v128)
+ * STACK_TYPE - type on stack (v128)
  * CP - CP(DST, SRC) to load and convert
+ * LS - lane bits
+ * NL - number of lanes
  */
 #define SIMD_LOADOP(NAME, MEM, STACK_TYPE, CP)                                \
+        SIMD_LOADOP_LANE(NAME, MEM, 128, 1, STACK_TYPE, CP)
+#define SIMD_LOADOP_LANE(NAME, MEM, LS, NL, STACK_TYPE, CP)                   \
         INSN_IMPL(NAME)                                                       \
         {                                                                     \
                 const struct module *m = MODULE;                              \
@@ -18,8 +43,15 @@
                 CHECK(memarg.memidx < m->nimportedmems + m->nmems);           \
                 CHECK(1 <= (MEM / 8) >>                                       \
                       memarg.align); /* 2 ** align <= N / 8 */                \
-                POP_VAL(TYPE_i32, i);                                         \
+                READ_LANEIDX##NL(lane);                                       \
                 struct val val_c;                                             \
+                if (NL != 1) {                                                \
+                        POP_VAL(TYPE_##STACK_TYPE, x);                        \
+                        if (EXECUTING) {                                      \
+                                val_c = val_x;                                \
+                        }                                                     \
+                }                                                             \
+                POP_VAL(TYPE_i32, i);                                         \
                 if (EXECUTING) {                                              \
                         void *datap;                                          \
                         ret = memory_getptr(ECTX, memarg.memidx, val_i.u.i32, \
@@ -27,7 +59,7 @@
                         if (ret != 0) {                                       \
                                 goto fail;                                    \
                         }                                                     \
-                        CP(&val_c.u.STACK_TYPE, datap);                       \
+                        CP(&LANEPTR##LS(&val_c)[lane], datap);                \
                 }                                                             \
                 PUSH_VAL(TYPE_##STACK_TYPE, c);                               \
                 SAVE_PC;                                                      \
@@ -78,7 +110,7 @@ fail:                                                                         \
                 p += 16;                                                      \
                 struct val val_v;                                             \
                 if (EXECUTING) {                                              \
-                        CP_V128(&val_v.u.v128, immp);                         \
+                        COPYBITS128(&val_v.u.v128, immp);                     \
                 }                                                             \
                 PUSH_VAL(TYPE_v128, v);                                       \
                 SAVE_PC;                                                      \
@@ -87,7 +119,12 @@ fail:                                                                         \
                 INSN_FAIL;                                                    \
         }
 
-#define CP_V128(a, b) memcpy(a, b, 128 / 8)
+#define COPYBITS(a, b, n) memcpy(a, b, n / 8)
+#define COPYBITS8(a, b) COPYBITS(a, b, 8)
+#define COPYBITS16(a, b) COPYBITS(a, b, 16)
+#define COPYBITS32(a, b) COPYBITS(a, b, 32)
+#define COPYBITS64(a, b) COPYBITS(a, b, 64)
+#define COPYBITS128(a, b) COPYBITS(a, b, 128)
 
 #define EXTEND_s(B, S) (int##B##_t)(S)
 #define EXTEND_u(B, S) (uint##B##_t)(S)
@@ -135,7 +172,7 @@ fail:                                                                         \
                 }                                                             \
         } while (0)
 
-SIMD_LOADOP(v128_load, 128, v128, CP_V128)
+SIMD_LOADOP(v128_load, 128, v128, COPYBITS128)
 SIMD_LOADOP(v128_load8x8_s, 64, v128, EXTEND_8x8_s)
 SIMD_LOADOP(v128_load8x8_u, 64, v128, EXTEND_8x8_u)
 SIMD_LOADOP(v128_load16x4_s, 64, v128, EXTEND_16x4_s)
@@ -160,6 +197,11 @@ SIMD_LOADOP(v128_load64_splat, 64, v128, SPLAT_64)
 SIMD_LOADOP(v128_load32_zero, 32, v128, ZERO32)
 SIMD_LOADOP(v128_load64_zero, 64, v128, ZERO64)
 
-SIMD_STOREOP(v128_store, 128, v128, CP_V128)
+SIMD_STOREOP(v128_store, 128, v128, COPYBITS128)
 
 SIMD_CONSTOP(v128_const)
+
+SIMD_LOADOP_LANE(v128_load8_lane, 8, 8, 16, v128, COPYBITS8)
+SIMD_LOADOP_LANE(v128_load16_lane, 16, 16, 8, v128, COPYBITS16)
+SIMD_LOADOP_LANE(v128_load32_lane, 32, 32, 4, v128, COPYBITS32)
+SIMD_LOADOP_LANE(v128_load64_lane, 64, 64, 2, v128, COPYBITS64)
