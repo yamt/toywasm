f3e27b751189b5b1f0a2d47086695fa216f9a310
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-05-19 20:22:23 +0900

    Revert "bump wasm-spec-test version"
    This reverts commit ac1c709c4e86c807293dd8910341bfebbc0d18fc.
    
    i bumped the version of a wrong repo

--- test/fetch-spec-test.sh
+++ test/fetch-spec-test.sh
@@ -6,7 +6,7 @@ TEST_DIR=${TEST_DIR:-.wasm-spec-test}
 fetch_spec_json()
 {
     REPO=https://github.com/yamt/wasm-spec-test
-    REF=cf0d19c6d687b702667c105c6036cd4d8aa18678
+    REF=29ab36f9ea07bbb3e329e4a19393567a1782039e
     mkdir "${TEST_DIR}"
     git -C "${TEST_DIR}" init
     git -C "${TEST_DIR}" fetch --depth 1 ${REPO} ${REF}
