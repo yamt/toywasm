ecab25a330ab9cfa6dee111c5bf1fd6494a4ee4e
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-09 23:06:23 +0900

    constify a bit

--- lib/module.h
+++ lib/module.h
@@ -7,8 +7,8 @@ struct name;
 int module_create(struct module **mp, const uint8_t *p, const uint8_t *ep,
                   struct load_context *ctx);
 void module_destroy(struct module *m);
-int module_find_export(struct module *m, const struct name *name,
+int module_find_export(const struct module *m, const struct name *name,
                        uint32_t type, uint32_t *idxp);
-int module_find_export_func(struct module *m, const struct name *name,
+int module_find_export_func(const struct module *m, const struct name *name,
                             uint32_t *funcidxp);
 void module_print_stats(const struct module *m);
