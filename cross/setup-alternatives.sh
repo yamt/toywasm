e788ca01e53746e7c4526e2af644e0b294ae8426
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-25 14:12:20 +0900

    setup-alternatives.sh: remove existing groups

--- cross/setup-alternatives.sh
+++ cross/setup-alternatives.sh
@@ -6,6 +6,7 @@ LLVM_VERSION=${LLVM_VERSION:-13}
 ALT_PRIO=100
 
 for c in clang llvm-ar llvm-ranlib ld.lld lld; do
+    update-alternatives --remove-all ${c} || :
     update-alternatives --install \
         /usr/bin/${c} \
         ${c} \
