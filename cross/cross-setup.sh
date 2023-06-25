86af62c9fb6974514ae156719c8a7a71ff8e8493
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-25 12:56:41 +0900

    cross-setup.sh: install clang-13 as well

--- cross/cross-setup.sh
+++ cross/cross-setup.sh
@@ -4,7 +4,7 @@ set -e
 
 dpkg --add-architecture ${ARCH}
 apt update
-apt install -y crossbuild-essential-${ARCH} libcmocka-dev:${ARCH} wabt llvm-13
+apt install -y crossbuild-essential-${ARCH} libcmocka-dev:${ARCH} wabt clang-13
 
 if [ ${ARCH} = i386 ]; then
     apt-get install -y libc6-dev:i386
