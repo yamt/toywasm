e22706abc0d2f571392a51fd0175dac4f04c3893
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-25 12:48:34 +0900

    setup-focal-proposed.sh: only for amd64
    it seems that some of other ports use a different url.

--- cross/setup-focal-proposed.sh
+++ cross/setup-focal-proposed.sh
@@ -5,7 +5,7 @@
 # https://manpages.ubuntu.com/manpages/focal/man5/apt_preferences.5.html
 
 cat <<EOF > /etc/apt/sources.list.d/ubuntu-focal-proposed.list
-deb http://archive.ubuntu.com/ubuntu/ focal-proposed restricted main multiverse universe
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ focal-proposed restricted main multiverse universe
 EOF
 
 cat <<EOF > /etc/apt/preferences.d/proposed-updates
