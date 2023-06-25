fc2d9e9bc053003006d746c02d6b495d0ecc5f58
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-25 13:51:13 +0900

    separate setup-alternatives.sh

--- cross/cross-setup.sh
+++ cross/cross-setup.sh
@@ -3,7 +3,6 @@
 set -e
 
 LLVM_VERSION=13
-ALT_PRIO=100
 
 dpkg --add-architecture ${ARCH}
 apt update
@@ -12,11 +11,3 @@ apt install -y crossbuild-essential-${ARCH} libcmocka-dev:${ARCH} wabt clang-${L
 if [ ${ARCH} = i386 ]; then
     apt-get install -y libc6-dev:i386
 fi
-
-for c in clang llvm-ar llvm-ranlib ld.lld lld; do
-    update-alternatives --install \
-        /usr/bin/${c} \
-        ${c} \
-        /usr/bin/${c}-${LLVM_VERSION} \
-        ${ALT_PRIO}
-done
---
+++ cross/setup-alternatives.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+set -e
+
+LLVM_VERSION=${LLVM_VERSION:-13}
+ALT_PRIO=100
+
+for c in clang llvm-ar llvm-ranlib ld.lld lld; do
+    update-alternatives --install \
+        /usr/bin/${c} \
+        ${c} \
+        /usr/bin/${c}-${LLVM_VERSION} \
+        ${ALT_PRIO}
+done
