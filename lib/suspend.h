4f9972d30919b852539506ca3e1138633da0e409
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-28 01:38:14 +0900

    comment

--- lib/suspend.h
+++ lib/suspend.h
@@ -1,7 +1,38 @@
 struct cluster;
 struct exec_context;
 
+/*
+ * supend_check_interrupt: used by check_interrupt() to check
+ * pending suspend requests.
+ */
 int suspend_check_interrupt(struct exec_context *ctx, const struct cluster *c);
+
+/*
+ * suspend_parked: called by each thread when it's suspended.
+ *
+ * called by instance_execute_handle_restart or its variations.
+ * threads are blocked within this function until its execution is resumed.
+ */
 void suspend_parked(struct cluster *c);
+
+/*
+ * suspend_threads: request to suspend all threads in the cluster.
+ *
+ * when suspended, every threads in the cluster will either
+ * a) block in suspend_threads
+ * b) rewind the stack (by returning a restartable error) and block
+ *    in suspend_parked.
+ *
+ * suspend_threads works as a cluster-global mutex as well.
+ * that is, only one thread can execute the code between suspend_threads
+ * and resume_threads in the same time.
+ *
+ * Note: this is a no-op for TOYWASM_USE_USER_SCHED=ON.
+ */
 void suspend_threads(struct cluster *c);
+
+/*
+ * resume_threads: request to undo resume_threads.
+ * should only be called after a successful call of suspend_threads().
+ */
 void resume_threads(struct cluster *c);
