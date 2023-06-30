yamt/toywasm

commit b112428647aa1427a159f8090e98fb3e2673e10e

a hack to avoid --timeout causing a busy loop

lib/exec.c
@@ -907,9 +907,14 @@ check_interrupt(struct exec_context *ctx)
          * practically it shouldn't be a problem though.
          */
         if (ctx->intrp != NULL && *ctx->intrp != 0) {
-                xlog_trace("get interrupt");
-                STAT_INC(ctx->stats.interrupt_user);
-                return ETOYWASMUSERINTERRUPT;
+                if (ctx->user_intr_delay_count < ctx->user_intr_delay) {
+                        ctx->user_intr_delay_count++;
+                } else {
+                        ctx->user_intr_delay_count = 0;
+                        xlog_trace("get interrupt");
+                        STAT_INC(ctx->stats.interrupt_user);
+                        return ETOYWASMUSERINTERRUPT;
+                }
         }
 #if defined(TOYWASM_ENABLE_WASM_THREADS)
         if (ctx->cluster != NULL) {

cli/repl.c
@@ -835,6 +835,12 @@ exec_func(struct exec_context *ctx, uint32_t funcidx,
                  * signals.
                  */
                 ctx->intrp = &one;
+                /*
+                 * a hack to avoid busy loop.
+                 * maybe it's cleaner for us to provide a timer functionality
+                 * by ourselves. but i feel it's too much for now.
+                 */
+                ctx->user_intr_delay = 1;
         }
         ret = instance_execute_func(ctx, funcidx, ptype, rtype, param, result);
         do {

lib/exec_context.h
@@ -196,6 +196,8 @@ struct exec_context {
          */
         const atomic_uint *intrp;
         struct cluster *cluster;
+        unsigned int user_intr_delay_count;
+        unsigned int user_intr_delay;
 
         /* scheduler */
         struct sched *sched;

commit ff5958d38810d3bd9fbf132c2430f3bceb572b97

check_interrupt: add a branch hint
for some reasons this restores about 5% performance regression for
test/run-ffmpeg.sh
(the regression was introduced by b112428647aa1427a159f8090e98fb3e2673e10e)

i'm not sure how this small change can affect the performance this much.
especially when i've heard that recent processors don't use this simple
pentium4 style "use jump directions as branch hints" prediction anymore.
(here in my environment "2.2 GHz 6-Core Intel Core i7" is used)

without this
```
spacetanuki% ./test/run-ffmpeg.sh /usr/bin/time -l ./b/toywasm --wasi --wasi-dir=.video --
       83.77 real        83.71 user         0.04 sys
```
```
0000000100006e50 <_check_interrupt>:
100006e50: 48 8b 8f d0 00 00 00         movq    208(%rdi), %rcx
100006e57: 31 c0                        xorl    %eax, %eax
100006e59: 48 85 c9                     testq   %rcx, %rcx
100006e5c: 74 35                        je      0x100006e93 <_check_interrupt+0x43>
100006e5e: 8b 09                        movl    (%rcx), %ecx
100006e60: 85 c9                        testl   %ecx, %ecx
100006e62: 74 2f                        je      0x100006e93 <_check_interrupt+0x43>
100006e64: 8b 8f e0 00 00 00            movl    224(%rdi), %ecx
100006e6a: 3b 8f e4 00 00 00            cmpl    228(%rdi), %ecx
100006e70: 73 0a                        jae     0x100006e7c <_check_interrupt+0x2c>
100006e72: 83 c1 01                     addl    $1, %ecx
100006e75: 89 8f e0 00 00 00            movl    %ecx, 224(%rdi)
100006e7b: c3                           retq
100006e7c: c7 87 e0 00 00 00 00 00 00 00        movl    $0, 224(%rdi)
100006e86: 48 83 87 e8 01 00 00 01      addq    $1, 488(%rdi)
100006e8e: b8 fd ff ff ff               movl    $4294967293, %eax       ## imm = 0xFFFFFFFD
100006e93: c3                           retq
100006e94: 66 2e 0f 1f 84 00 00 00 00 00        nopw    %cs:(%rax,%rax)
100006e9e: 66 90                        nop
```

with this
```

lib/exec.c
@@ -940,7 +940,7 @@ check_interrupt(struct exec_context *ctx)
          * theoretically we probably need a memory barrier.
          * practically it shouldn't be a problem though.
          */
-        if (ctx->intrp != NULL && *ctx->intrp != 0) {
+        if (ctx->intrp != NULL && __predict_false(*ctx->intrp != 0)) {
                 if (ctx->user_intr_delay_count < ctx->user_intr_delay) {
                         ctx->user_intr_delay_count++;
                 } else {