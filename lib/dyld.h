aff651e440eb1f268f7cad4fe57cf6402b2cd7e7
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-13 00:40:52 +0900

    wip dyld

---
+++ lib/dyld.h
@@ -0,0 +1,28 @@
+struct dyld_plt {
+        struct finst *finst;
+        struct tableinst *tableinst;
+        uint32_t idx_in_table;
+};
+
+struct dyld_object {
+        uint32_t memory_base;
+        uint32_t table_base;
+
+        const uint8_t *bin;
+        size_t binsz;
+        struct module *module;
+        struct instance *instance;
+
+        struct dyld_object *next;
+};
+
+struct dyld {
+        uint32_t memory_base;
+        uint32_t table_base;
+        struct meminst *meminst;
+        struct tableinst *tableinst;
+        struct dyld_object *objs;
+};
+
+int dyld_resolve_symbol(dyld_object *refobj, enum importtype type,
+                        const struct name *name, void **resultp);
