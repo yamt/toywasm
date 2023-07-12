79cb6b1d139c35000f7fdc52105b3d0757ddc554
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-13 01:04:57 +0900

    wip dyld

--- lib/dyld.h
+++ lib/dyld.h
@@ -1,5 +1,14 @@
 #include <stdint.h>
 
+#include "list.h"
+#include "type.h"
+
+struct dyld_sym {
+		const struct name *name;
+        enum importtype type;
+        uint32_t idx;
+};
+
 struct dyld_plt {
         struct finst *finst;
         struct tableinst *tableinst;
@@ -15,18 +24,24 @@ struct dyld_object {
         struct module *module;
         struct instance *instance;
 
-        struct dyld_object *next;
+        LIST_ENTRY(struct dyld_object) q;
 };
 
 struct dyld {
         uint32_t memory_base;
         uint32_t table_base;
+
+        struct memtype mt;
         struct meminst *meminst;
+
+        struct tabletype tt;
         struct tableinst *tableinst;
-        struct dyld_object *objs;
+
+        LIST_HEAD(struct dyld_object) objs;
 };
 
 #if 0
 int dyld_resolve_symbol(struct dyld_object *refobj, enum importtype type,
                         const struct name *name, void **resultp);
 #endif
+int dyld_load_main_object_from_file(struct dyld *d, const char *name);
