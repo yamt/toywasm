ae15eb4f29d3ac855f331bfc5f16216bb6e3e54a
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-16 06:36:42 +0900

    parse dylink.0 import_info

--- lib/dylink_type.h
+++ lib/dylink_type.h
@@ -12,9 +12,19 @@ struct dylink_needs {
         struct name *names;
 };
 
+struct dylink_import_info {
+        struct name module_name;
+        struct name name;
+        uint32_t flags; /* WASM_SYM_xxx */
+};
+
+#define WASM_SYM_BINDING_WEAK 1
+
 struct dylink {
         struct dylink_mem_info mem_info;
         struct dylink_needs needs;
+        uint32_t nimport_info;
+        struct dylink_import_info *import_info;
 };
 
 void clear_dylink(struct dylink *dy);
