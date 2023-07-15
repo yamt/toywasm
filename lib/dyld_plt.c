c371fc1e9639386762ff037aaa992ce1ec98fd47
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-16 04:10:55 +0900

    wip dyld

--- lib/dyld_plt.c
+++ lib/dyld_plt.c
@@ -36,12 +36,5 @@ dyld_plt(struct exec_context *ctx, struct host_instance *hi,
 
         ctx->event_u.call.func = plt->finst;
         ctx->event = EXEC_EVENT_CALL;
-
-        /* cancel the lsize adustment in do_host_call */
-        uint32_t nparams = resulttype_cellsize(&ft->parameter);
-        uint32_t nresults = resulttype_cellsize(&ft->result);
-        ctx->stack.lsize += nparams;
-        ctx->stack.lsize -= nresults;
-
-        return 0;
+        return ETOYWASMRESTART;
 }
