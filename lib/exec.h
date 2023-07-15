e76804dd3df20ecf732cfd2ce71b1bcfaa93f781
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-16 06:37:35 +0900

    add table_get_func

--- lib/exec.h
+++ lib/exec.h
@@ -13,6 +13,7 @@ struct funcinst;
 struct tableinst;
 struct meminst;
 struct globalinst;
+struct functype;
 struct localtype;
 struct resulttype;
 struct val;
@@ -44,6 +45,9 @@ int table_access(struct exec_context *ectx, uint32_t tableidx, uint32_t offset,
 void table_set(struct tableinst *tinst, uint32_t elemidx,
                const struct val *val);
 void table_get(struct tableinst *tinst, uint32_t elemidx, struct val *val);
+int table_get_func(struct exec_context *ectx, const struct tableinst *t,
+                   uint32_t i, const struct functype *ft,
+                   const struct funcinst **fip);
 int table_grow(struct tableinst *tinst, const struct val *val, uint32_t n);
 void global_set(struct globalinst *ginst, const struct val *val);
 void global_get(struct globalinst *ginst, struct val *val);
