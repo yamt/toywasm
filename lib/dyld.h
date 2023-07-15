3b348a3c76415450c592dd0c7651ed15672228f6
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-15 23:39:46 +0900

    wip dyld

--- lib/dyld.h
+++ lib/dyld.h
@@ -43,7 +43,7 @@ struct dyld_object {
 };
 
 struct dyld {
-        struct import_object *global_import_obj;
+        struct import_object *shared_import_obj;
 
         uint32_t memory_base;
         uint32_t table_base;
