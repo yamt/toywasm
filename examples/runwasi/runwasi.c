16a5e058237b721da55790c1bd1431e012a175a1
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-19 20:52:44 +0900

    update runwasi example after constify changes

--- examples/runwasi/runwasi.c
+++ examples/runwasi/runwasi.c
@@ -12,7 +12,7 @@
 
 int
 runwasi(const char *filename, unsigned int ndirs, char **dirs,
-        unsigned int nenvs, char **envs, int argc, char **argv,
+        unsigned int nenvs, const char *const *envs, int argc, const char *const *argv,
         const int stdio_fds[3], uint32_t *wasi_exit_code_p)
 {
         struct module *m = NULL;
