a4884a4ebcc4c1c87247834f6cd0a89791c3d49e
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-24 22:39:09 +0900

    add run-wcpl.sh

---
+++ test/run-wcpl.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -e
+
+DIR=.wcpl
+
+fetch() {
+    REPO=https://github.com/false-schemers/wcpl
+    REF=458a542ca81fa7a8fd8c8eed38a32dce8ed45135
+    mkdir "${DIR}"
+    git -C "${DIR}" init
+    git -C "${DIR}" fetch --depth 1 ${REPO} ${REF}
+    git -C "${DIR}" checkout FETCH_HEAD
+}
+
+if [ ! -d ${DIR} ]; then
+    fetch
+fi
+
+TOYWASM=${TOYWASM:-toywasm}
+
+# https://github.com/false-schemers/wcpl#self-hosting
+set -x
+cd ${DIR}
+cc -o wcpl [wcpl].c
+./wcpl -o wcpl.wasm [wcpl].c
+${TOYWASM} --wasi --wasi-dir=. -- wcpl.wasm -o wcpl1.wasm [wcpl].c
+diff -s wcpl.wasm wcpl1.wasm
