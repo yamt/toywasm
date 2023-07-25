5a78a4820b7f8e91e1e903aa82fa5a315124cab3
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-26 01:54:46 +0900

    add LIST_INSERT_HEAD

--- lib/list.h
+++ lib/list.h
@@ -20,6 +20,7 @@ struct list_head {
 void list_head_init(struct list_head *h);
 void list_remove(struct list_head *h, void *elem, struct list_entry *e);
 void list_insert_tail(struct list_head *h, void *elem, struct list_entry *e);
+void list_insert_head(struct list_head *h, void *elem, struct list_entry *e);
 
 #define LIST_ENTRY(TYPE)                                                      \
         struct {                                                              \
@@ -86,6 +87,15 @@ void list_insert_tail(struct list_head *h, void *elem, struct list_entry *e);
         ctassert(sizeof(*(HEAD)) == sizeof(struct list_head));                \
         ctassert(sizeof((ELEM)->NAME) == sizeof(struct list_entry));          \
         list_insert_tail((struct list_head *)(HEAD), (ELEM),                  \
+                         (struct list_entry *)&(ELEM)->NAME)
+
+#define LIST_INSERT_HEAD(HEAD, ELEM, NAME)                                    \
+        CHECK_TYPE(&(HEAD)->first, (HEAD)->tailnextp);                        \
+        CHECK_TYPE((HEAD)->first, (ELEM)->NAME.next);                         \
+        CHECK_TYPE(&(ELEM)->NAME.next, (ELEM)->NAME.prevnextp);               \
+        ctassert(sizeof(*(HEAD)) == sizeof(struct list_head));                \
+        ctassert(sizeof((ELEM)->NAME) == sizeof(struct list_entry));          \
+        list_insert_head((struct list_head *)(HEAD), (ELEM),                  \
                          (struct list_entry *)&(ELEM)->NAME)
 
 #define LIST_HEAD_INIT(HEAD)                                                  \
