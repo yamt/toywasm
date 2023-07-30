18f13ad1eadf2a0d56068b97865ae347857c1930
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-30 10:05:24 +0900

    comment

--- examples/libdl/build.sh
+++ examples/libdl/build.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+# https://reviews.llvm.org/D153293
+# https://github.com/yamt/wasi-libc/tree/so or equivalent
+
 WASI_SDK=${WASI_SDK:-/opt/wasi-sdk}
 WASI_SYSROOT=${WASI_SYSROOT:-${WASI_SDK}/share/wasi-sysroot}
 LLVM_HOME=${LLVM_HOME:-${WASI_SDK}}
@@ -19,6 +22,7 @@ CFLAGS="${CFLAGS} -O3 -fPIC"
 # CFLAGS="${CFLAGS} -mtail-call"
 
 CLINKFLAGS="-Xlinker --experimental-pic"
+# https://reviews.llvm.org/D156205
 #CLIBLINKFLAGS="-shared -fvisibility=default -mexec-model=reactor"
 CLIBLINKFLAGS="-shared -fvisibility=default"
 
