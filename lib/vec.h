64a27067478fe6329182cde802d6d4a2aa35b6a3
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-08-09 22:39:26 +0900

    comment

--- lib/vec.h
+++ lib/vec.h
@@ -19,8 +19,18 @@ int __must_check _vec_prealloc(void *vec, size_t elem_size, uint32_t count);
 void _vec_free(void *vec);
 
 #define VEC_INIT(v) memset(&v, 0, sizeof(v))
+/*
+ * VEC_RESIZE resizes the size of vector. that is, psize == lsize == sz.
+ * when extending th vector, it initializes newly allocated elements
+ * with zeros.
+ */
 #define VEC_RESIZE(v, sz) _vec_resize(&v, sizeof(*v.p), sz);
-#define VEC_PREALLOC(v, sz) _vec_prealloc(&v, sizeof(*v.p), sz);
+/*
+ * VEC_PREALLOC ensures the vector to have enough tail room to store the
+ * given number of elements. that is, psize >= lsize + needed.
+ * it doesn't initialize newly allocated elements.
+ */
+#define VEC_PREALLOC(v, needed) _vec_prealloc(&v, sizeof(*v.p), needed);
 #define VEC_FREE(v) _vec_free(&v)
 #define VEC_FOREACH(it, v) ARRAY_FOREACH(it, v.p, v.lsize)
 #define VEC_FOREACH_IDX(i, it, v) for (i = 0, it = v.p; i < v.lsize; i++, it++)
