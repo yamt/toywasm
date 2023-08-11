5660c06802fcff425f9864cc3775a864c33d94c4
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-08-11 11:54:13 +0900

    move some stuff from dyld.h to dyld_impl.h

--- libdyld/dyld_impl.h
+++ libdyld/dyld_impl.h
@@ -1,8 +1,57 @@
 #include <stdbool.h>
 
+#include "list.h"
+#include "type.h"
+
 struct dyld;
-struct dyld_object;
-struct name;
+
+enum symtype {
+        SYM_TYPE_FUNC,
+        SYM_TYPE_MEM,
+};
+
+struct dyld_plt {
+        const struct funcinst *finst;
+        const struct name *sym;
+        struct dyld_object *refobj;
+        struct funcinst pltfi;
+};
+
+struct dyld_object {
+        const struct name *name;
+
+        uint32_t memory_base;
+        uint32_t table_base;
+        uint32_t table_export_base;
+
+        uint32_t nexports; /* # of reserved entries from table_export_base */
+
+        struct globalinst memory_base_global;
+        struct globalinst table_base_global;
+
+        uint32_t ngots;
+        struct globalinst *gots;
+        uint32_t nplts;
+        struct dyld_plt *plts;
+        struct import_object *local_import_obj;
+
+        const uint8_t *bin;
+        size_t binsz;
+        struct module *module;
+        struct instance *instance;
+
+        struct dyld *dyld;
+        LIST_ENTRY(struct dyld_object) q;
+
+        /* for tsort */
+        bool visited;
+        LIST_ENTRY(struct dyld_object) tq;
+};
+
+struct dyld_dynamic_object {
+        struct name name;
+        struct dyld_object *obj;
+};
 
 int dyld_resolve_dependencies(struct dyld *d, struct dyld_object *obj,
                               bool bindnow);
