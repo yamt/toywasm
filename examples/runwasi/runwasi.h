4c8e176b5b9e057aa612da3efd33c101f28c3bd9
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-10-31 20:13:38 +0900

    examples/runwasi: add an extra import object argument

--- examples/runwasi/runwasi.h
+++ examples/runwasi/runwasi.h
@@ -1,6 +1,8 @@
 #include <stdint.h>
 
+struct import_object;
+
 int runwasi(const char *filename, unsigned int ndirs, char **dirs,
             unsigned int nenvs, const char *const *envs, int argc,
             const char *const *argv, const int stdio_fds[3],
-            uint32_t *wasi_exit_code_p);
+            struct import_object *base_imports, uint32_t *wasi_exit_code_p);
