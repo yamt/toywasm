fc38ec5e86629383215abde808204e8f07cde91e
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-11-03 11:16:28 +0900

    comment

--- lib/restart.c
+++ lib/restart.c
@@ -2,6 +2,34 @@
 #include "exec_context.h"
 #include "vec.h"
 
+/*
+ * expected usage:
+ *
+ *  ret = restart_info_prealloc(ctx);
+ *  if (ret != 0) {
+ *      return ret;
+ *  }
+ *  struct restart_info *restart = &VEC_NEXTELEM(ctx->restarts);
+ *  if (restart->restart_type != RESTART_NONE) {
+ *      assert(restart->restart_type == RESTART_xxx);
+ *      // restore and continue the previous operation from
+ *      // restart->restart_u.u_xxx
+ *  }
+ *    :
+ *    :
+ *    :
+ *  if (...) {
+ *      restart->restart_type = RESTART_XXX;
+ *      // save the context to restart->restart_u.u_xxx
+ *      host_ret = ETOYWASMRESTART; // restartable error
+ *  } else {
+ *      restart_info_clear(ctx);
+ *      host_ret = 0; // success or a restartable error
+ *  }
+ *  assert(IS_RESTARTABLE(host_ret) || restart->restart_type == RESTART_NONE);
+ *  return host_ret;
+ */
+
 int
 restart_info_prealloc(struct exec_context *ctx)
 {
