ea5928df0b4ad5186e592822d6c6c7ce2b13ad3c
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-11-22 22:29:17 +0900

    build-toywasm-and-app.sh: honor EXTRA_CMAKE_OPTIONS

--- examples/build-toywasm-and-app.sh
+++ examples/build-toywasm-and-app.sh
@@ -8,7 +8,7 @@ echo "ROOT: ${ROOT}"
 BUILD_TOYWASM=${ROOT}/build-toywasm
 BUILD_APP=${ROOT}/build-app
 
-cmake -G Ninja -B ${BUILD_TOYWASM} -D CMAKE_INSTALL_PREFIX=${ROOT} ../..
+cmake -G Ninja -B ${BUILD_TOYWASM} -D CMAKE_INSTALL_PREFIX=${ROOT} ${EXTRA_CMAKE_OPTIONS} ../..
 cmake --build ${BUILD_TOYWASM} --target install
-cmake -G Ninja -B ${BUILD_APP} -D CMAKE_PREFIX_PATH=${ROOT} .
+cmake -G Ninja -B ${BUILD_APP} -D CMAKE_PREFIX_PATH=${ROOT} ${EXTRA_CMAKE_OPTIONS} .
 cmake --build ${BUILD_APP}
