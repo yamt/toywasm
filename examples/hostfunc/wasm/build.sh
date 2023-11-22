ac721bb806d1c7628895e25829b54c50179177dd
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-11-22 23:23:18 +0900

    examples/hostfunc/wasm/build.sh: build more combinations

--- examples/hostfunc/wasm/build.sh
+++ examples/hostfunc/wasm/build.sh
@@ -2,23 +2,45 @@
 
 CC=/opt/wasi-sdk/bin/clang
 
-${CC} -Xlinker --export-table \
+${CC} \
 -o test.wasm \
 test.c
 
-${CC} -Xlinker --export-table \
+${CC} \
 -o test.host-func1.wasm \
--DUSE_HOST_LOAD_CALL_ADD \
+-DUSE_HOST_LOAD \
 test.c
 
 ${CC} -Xlinker --export-table \
 -o test.host-func2.wasm \
 -DUSE_HOST_LOAD_CALL \
--DUSE_HOST_LOAD_CALL_ADD \
 test.c
 
 ${CC} -Xlinker --export-table \
 -o test.host-func3.wasm \
+-DUSE_HOST_LOAD \
+-DUSE_HOST_LOAD_CALL \
+test.c
+
+${CC} -Xlinker --export-table \
+-o test.host-func4.wasm \
+-DUSE_HOST_LOAD_CALL_ADD \
+test.c
+
+${CC} -Xlinker --export-table \
+-o test.host-func5.wasm \
+-DUSE_HOST_LOAD \
+-DUSE_HOST_LOAD_CALL_ADD \
+test.c
+
+${CC} -Xlinker --export-table \
+-o test.host-func6.wasm \
+-DUSE_HOST_LOAD_CALL \
+-DUSE_HOST_LOAD_CALL_ADD \
+test.c
+
+${CC} -Xlinker --export-table \
+-o test.host-func7.wasm \
 -DUSE_HOST_LOAD \
 -DUSE_HOST_LOAD_CALL \
 -DUSE_HOST_LOAD_CALL_ADD \
