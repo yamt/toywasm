5c2942bafdb045443d389137c55a29534a904da8
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-01-28 13:08:23 +0900

    fix a few opcode names

--- lib/insn_list_eh.h
+++ lib/insn_list_eh.h
@@ -1,7 +1,7 @@
 /* clang-format off */
 
 INSTRUCTION(0x08, "throw", throw, 0)
-INSTRUCTION(0x0a, "throw-ref", throw_ref, 0)
-INSTRUCTION(0x1f, "try-table", try_table, 0)
+INSTRUCTION(0x0a, "throw_ref", throw_ref, 0)
+INSTRUCTION(0x1f, "try_table", try_table, 0)
 
 /* clang-format on */
