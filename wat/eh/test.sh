8db499ba13776badc215c7ad59a41e079e1f7499
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-01-28 20:13:36 +0900

    wat/eh/test.sh: test trapping case as well

--- wat/eh/test.sh
+++ wat/eh/test.sh
@@ -3,7 +3,13 @@
 set -e
 set -x
 TOYWASM=${TOYWASM:-${TEST_RUNTIME_EXE:-toywasm}}
+
 for wat in *.wat; do
     wasm=${wat%%.wat}.wasm
     ${TOYWASM} ${wasm}
+done
+
+for wat in trap/*.wat; do
+    wasm=${wat%%.wat}.wasm
+    ${TOYWASM} ${wasm} 2>&1 | grep '\[trap\]'
 done
