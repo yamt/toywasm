e6ca9944e958946ce4bc15bacb6405e4f3ef9b86
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-18 17:11:36 +0900

    libwasi: stop using mode_t

--- libwasi/wasi_host_subr.h
+++ libwasi/wasi_host_subr.h
@@ -13,7 +13,7 @@ uint64_t timespec_to_ns(const struct timespec *ts);
 void timeval_from_ns(struct timeval *tv, uint64_t ns);
 int prepare_utimes_tv(const struct utimes_args *args, struct timeval *tvstore,
                       const struct timeval **resultp);
-uint8_t wasi_convert_filetype(mode_t mode);
+uint8_t wasi_convert_filetype(unsigned int mode);
 void wasi_convert_filestat(const struct stat *hst, struct wasi_filestat *wst);
 int wasi_unstable_convert_filestat(const struct wasi_filestat *wst,
                                    struct wasi_unstable_filestat *uwst);
