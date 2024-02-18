57e77c498c7af8ce1e91aa46c0df5d45909561ba
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-18 16:50:17 +0900

    libwasi: rename wasi_host_ api prefix to wasi_host_path_

--- libwasi/wasi_dirop.h
+++ libwasi/wasi_dirop.h
@@ -6,19 +6,19 @@ struct path_info {
 struct wasi_filestat;
 struct utimes_args;
 
-int wasi_host_open(const struct path_info *pi, int oflags, unsigned int mode,
+int wasi_host_path_open(const struct path_info *pi, int oflags, unsigned int mode,
                    int *fdp);
-int wasi_host_unlink(const struct path_info *pi);
-int wasi_host_mkdir(const struct path_info *pi);
-int wasi_host_rmdir(const struct path_info *pi);
-int wasi_host_symlink(const char *target_buf, const struct path_info *pi);
-int wasi_host_readlink(const struct path_info *pi, char *buf, size_t buflen,
+int wasi_host_path_unlink(const struct path_info *pi);
+int wasi_host_path_mkdir(const struct path_info *pi);
+int wasi_host_path_rmdir(const struct path_info *pi);
+int wasi_host_path_symlink(const char *target_buf, const struct path_info *pi);
+int wasi_host_path_readlink(const struct path_info *pi, char *buf, size_t buflen,
                        size_t *resultp);
-int wasi_host_link(const struct path_info *pi1, const struct path_info *pi2);
-int wasi_host_rename(const struct path_info *pi1, const struct path_info *pi2);
-int wasi_host_stat(const struct path_info *pi, struct wasi_filestat *stp);
-int wasi_host_lstat(const struct path_info *pi, struct wasi_filestat *stp);
-int wasi_host_utimes(const struct path_info *pi,
+int wasi_host_path_link(const struct path_info *pi1, const struct path_info *pi2);
+int wasi_host_path_rename(const struct path_info *pi1, const struct path_info *pi2);
+int wasi_host_path_stat(const struct path_info *pi, struct wasi_filestat *stp);
+int wasi_host_path_lstat(const struct path_info *pi, struct wasi_filestat *stp);
+int wasi_host_path_utimes(const struct path_info *pi,
                      const struct utimes_args *args);
-int wasi_host_lutimes(const struct path_info *pi,
+int wasi_host_path_lutimes(const struct path_info *pi,
                       const struct utimes_args *args);
