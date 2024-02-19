d118e40a30f2237c435b25164bb06b74647b0345
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-19 16:53:43 +0900

    libwasi: move clock and random to separate files

---
+++ libwasi/wasi_clock.c
@@ -0,0 +1,85 @@
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "endian.h"
+#include "wasi_host_subr.h"
+#include "wasi_impl.h"
+#include "xlog.h"
+
+#include "wasi_hostfuncs.h"
+
+int
+wasi_clock_res_get(struct exec_context *ctx, struct host_instance *hi,
+                   const struct functype *ft, const struct cell *params,
+                   struct cell *results)
+{
+        WASI_TRACE;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t clockid = HOST_FUNC_PARAM(ft, params, 0, i32);
+        uint32_t retp = HOST_FUNC_PARAM(ft, params, 1, i32);
+        clockid_t hostclockid;
+        int host_ret = 0;
+        int ret;
+        ret = wasi_convert_clockid(clockid, &hostclockid);
+        if (ret != 0) {
+                goto fail;
+        }
+        struct timespec ts;
+        ret = clock_getres(hostclockid, &ts);
+        if (ret == -1) {
+                ret = errno;
+                assert(ret > 0);
+                goto fail;
+        }
+        uint64_t result = host_to_le64(timespec_to_ns(&ts));
+        host_ret = wasi_copyout(ctx, &result, retp, sizeof(result),
+                                WASI_U64_ALIGN);
+fail:
+        if (host_ret == 0) {
+                HOST_FUNC_RESULT_SET(ft, results, 0, i32,
+                                     wasi_convert_errno(ret));
+        }
+        HOST_FUNC_FREE_CONVERTED_PARAMS();
+        return host_ret;
+}
+
+int
+wasi_clock_time_get(struct exec_context *ctx, struct host_instance *hi,
+                    const struct functype *ft, const struct cell *params,
+                    struct cell *results)
+{
+        WASI_TRACE;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t clockid = HOST_FUNC_PARAM(ft, params, 0, i32);
+#if 0 /* REVISIT what to do with the precision? */
+        uint64_t precision = HOST_FUNC_PARAM(ft, params, 1, i64);
+#endif
+        uint32_t retp = HOST_FUNC_PARAM(ft, params, 2, i32);
+        clockid_t hostclockid;
+        int host_ret = 0;
+        int ret;
+        ret = wasi_convert_clockid(clockid, &hostclockid);
+        if (ret != 0) {
+                goto fail;
+        }
+        struct timespec ts;
+        ret = clock_gettime(hostclockid, &ts);
+        if (ret == -1) {
+                ret = errno;
+                assert(ret > 0);
+                goto fail;
+        }
+        uint64_t result = host_to_le64(timespec_to_ns(&ts));
+        host_ret = wasi_copyout(ctx, &result, retp, sizeof(result),
+                                WASI_U64_ALIGN);
+fail:
+        if (host_ret == 0) {
+                HOST_FUNC_RESULT_SET(ft, results, 0, i32,
+                                     wasi_convert_errno(ret));
+        }
+        HOST_FUNC_FREE_CONVERTED_PARAMS();
+        return host_ret;
+}
---
+++ libwasi/wasi_random.c
@@ -0,0 +1,69 @@
+#define _DARWIN_C_SOURCE
+#define _GNU_SOURCE
+#define _NETBSD_SOURCE
+
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__GLIBC__) || defined(__NuttX__)
+#include <sys/random.h> /* getrandom */
+#endif
+
+#include "endian.h"
+#include "exec.h"
+#include "wasi_impl.h"
+#include "xlog.h"
+
+#include "wasi_hostfuncs.h"
+
+int
+wasi_random_get(struct exec_context *ctx, struct host_instance *hi,
+                const struct functype *ft, const struct cell *params,
+                struct cell *results)
+{
+        WASI_TRACE;
+        HOST_FUNC_CONVERT_PARAMS(ft, params);
+        uint32_t buf = HOST_FUNC_PARAM(ft, params, 0, i32);
+        uint32_t buflen = HOST_FUNC_PARAM(ft, params, 1, i32);
+        int ret = 0;
+        void *p;
+        int host_ret = memory_getptr(ctx, 0, buf, 0, buflen, &p);
+        if (host_ret != 0) {
+                goto fail;
+        }
+#if defined(__GLIBC__) || defined(__NuttX__)
+        /*
+         * glibc doesn't have arc4random
+         * https://sourceware.org/bugzilla/show_bug.cgi?id=4417
+         *
+         * NuttX has both of getrandom and arc4random_buf.
+         * The latter is available only if CONFIG_CRYPTO_RANDOM_POOL=y.
+         */
+        while (buflen > 0) {
+                ssize_t ssz = getrandom(p, buflen, 0);
+                if (ssz == -1) {
+                        ret = errno;
+                        assert(ret > 0);
+                        if (ret == EINTR) {
+                                continue;
+                        }
+                        break;
+                }
+                p = (uint8_t *)p + ssz;
+                buflen -= ssz;
+        }
+#else
+        arc4random_buf(p, buflen);
+        ret = 0;
+#endif
+fail:
+        if (host_ret == 0) {
+                HOST_FUNC_RESULT_SET(ft, results, 0, i32,
+                                     wasi_convert_errno(ret));
+        }
+        HOST_FUNC_FREE_CONVERTED_PARAMS();
+        return host_ret;
+}
