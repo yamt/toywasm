cfe8b2620372435a7f45f8f72492d9f698db6bf6
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-22 16:01:28 +0900

    libwasi: keep fdinfo in path_info

--- libwasi/wasi_path_subr.h
+++ libwasi/wasi_path_subr.h
@@ -1,17 +1,21 @@
+struct wasi_fdinfo;
+
 struct path_info {
         char *hostpath;
+
+        struct wasi_fdinfo *dirfdinfo;
         char *wasmpath;
 };
 
 #define PATH_INITIALIZER                                                      \
         {                                                                     \
-                NULL, NULL,                                                   \
+                NULL, NULL, NULL,                                             \
         }
 
 struct exec_context;
 struct wasi_instance;
 
-void path_clear(struct path_info *pi);
+void path_clear(struct wasi_instance *wasi, struct path_info *pi);
 int wasi_copyin_and_convert_path(struct exec_context *ctx,
                                  struct wasi_instance *wasi,
                                  uint32_t dirwasifd, uint32_t path,
