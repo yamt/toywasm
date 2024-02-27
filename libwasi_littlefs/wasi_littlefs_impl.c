7c3ec1278eaa66c58cfd044328fb48b671d887c8
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-28 00:25:43 +0900

    libwasi_littlefs: fix lfs_error_to_errno

--- libwasi_littlefs/wasi_littlefs_impl.c
+++ libwasi_littlefs/wasi_littlefs_impl.c
@@ -11,34 +11,49 @@ lfs_error_to_errno(enum lfs_error lfs_error)
         switch (lfs_error) {
         case LFS_ERR_OK:
                 error = 0;
+                break;
         case LFS_ERR_IO:
                 error = EIO;
+                break;
         case LFS_ERR_CORRUPT:
                 error = EIO; /* XXX */
+                break;
         case LFS_ERR_NOENT:
                 error = ENOENT;
+                break;
         case LFS_ERR_EXIST:
                 error = EEXIST;
+                break;
         case LFS_ERR_NOTDIR:
                 error = ENOTDIR;
+                break;
         case LFS_ERR_ISDIR:
                 error = EISDIR;
+                break;
         case LFS_ERR_NOTEMPTY:
                 error = ENOTEMPTY;
+                break;
         case LFS_ERR_BADF:
                 error = EBADF;
+                break;
         case LFS_ERR_FBIG:
                 error = EFBIG;
+                break;
         case LFS_ERR_INVAL:
                 error = EINVAL;
+                break;
         case LFS_ERR_NOSPC:
                 error = ENOSPC;
+                break;
         case LFS_ERR_NOMEM:
                 error = ENOMEM;
+                break;
         case LFS_ERR_NOATTR:
                 error = ENOENT; /* XXX */
+                break;
         case LFS_ERR_NAMETOOLONG:
                 error = ENAMETOOLONG;
+                break;
         }
         return error;
 }
