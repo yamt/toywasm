823319047d2f15c51b59199bbe5d675578a35f44
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-01 10:24:02 +0900

    libwasi_littlefs: littlefs stats

--- libwasi_littlefs/wasi_littlefs_impl.h
+++ libwasi_littlefs/wasi_littlefs_impl.h
@@ -25,7 +25,29 @@ struct wasi_vfs_lfs {
         lfs_t lfs;
         struct lfs_config lfs_config;
         int fd;
+#if defined(TOYWASM_ENABLE_LITTLEFS_STATS)
+        struct {
+                uint64_t bd_read;
+                uint64_t bd_read_bytes;
+                uint64_t bd_prog;
+                uint64_t bd_prog_bytes;
+                uint64_t bd_erase;
+                uint64_t bd_sync;
+        } stat;
+#endif
 };
+
+#if defined(TOYWASM_ENABLE_LITTLEFS_STATS)
+#define LFS_STAT_INC(st) (st)++
+#define LFS_STAT_ADD(st, x) (st) += (x)
+#else
+#define LFS_STAT_INC(st)                                                      \
+        do {                                                                  \
+        } while (0)
+#define LFS_STAT_ADD(st, x)                                                   \
+        do {                                                                  \
+        } while (0)
+#endif
 
 int lfs_error_to_errno(enum lfs_error lfs_error);
 
