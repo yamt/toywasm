d2d81a79d419c95ca3d4369f0440cc87a2733944
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-03 12:44:34 +0900

    update dyld test

--- test/dyld/fetch-and-run-test.sh
+++ test/dyld/fetch-and-run-test.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ ! -d wasi-shlib-bin ]; then
-    curl -L https://github.com/yamt/garbage/releases/download/wasi-shlib-bin-20240302/wasi-shlib-bin.tgz | pax -rvz
+    curl -L https://github.com/yamt/garbage/releases/download/wasi-shlib-bin-20240303/wasi-shlib-bin.tgz | pax -rvz
 fi
 cd wasi-shlib-bin
 exec ./test.sh
