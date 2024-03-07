d395700096a3f847fc00f6b1cfe2c9db2d878953
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-08 00:35:30 +0900

    wasi-testsuite-adapter.py: add littlefs support

--- test/wasi-testsuite-adapter.py
+++ test/wasi-testsuite-adapter.py
@@ -10,6 +10,13 @@ import signal
 import subprocess
 
 
+fstype = os.getenv("TOYWASM_WASI_FILESYSTEM_TYPE", "host")
+if fstype == "littlefs":
+    import pathlib
+    from littlefs import LittleFS
+    import tempfile
+
+
 def handler(a, b):
     sys.exit(1)  # this terminates subrpocess.run
 
@@ -38,10 +45,26 @@ if args.version:
     sys.exit(result.returncode)
 
 options = []
+images = []
 for x in args.env:
     options.extend([f"--wasi-env={x}"])
 for x in args.dir:
-    options.extend([f"--wasi-dir={x}"])
+    if fstype == "littlefs":
+        fs = LittleFS(block_size=4096, block_count=1000)
+        source_dir = pathlib.Path(x)
+        fs.mkdir(source_dir.as_posix())
+        for p in source_dir.rglob("*"):
+           if p.is_dir():
+               fs.mkdir(p.as_posix())
+           else:
+               with fs.open(p.as_posix(), "wb") as fp:
+                   fp.write(p.read_bytes())
+        image = tempfile.NamedTemporaryFile()
+        image.write(fs.context.buffer)
+        images.append(image)
+        options.extend([f"--wasi-littlefs-dir={image.name}::{x}"])
+    else:
+        options.extend([f"--wasi-dir={x}"])
 result = subprocess.run(
     shlex.split(executable) + ["--wasi"] + options + ["--", args.test_file] + args.arg
 )
