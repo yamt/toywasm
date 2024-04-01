74e2d8bbd0f0ab3887088d768a593e0557736d9f
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-04-01 22:34:26 +0900

    libwasi: remove unnecessary header inclusions

--- libwasi/wasi_abi_path.c
+++ libwasi/wasi_abi_path.c
@@ -1,11 +1,8 @@
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "endian.h"
 #include "exec.h"
