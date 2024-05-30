eb7da8eac935c12f446ccffc5943ffc4b7c6fe4b
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-05-30 21:28:33 +0900

    add report_getmessage
    while i don't like to have accessor functions in general,
    this instance would be convenient for NULL check.

--- lib/report.h
+++ lib/report.h
@@ -16,6 +16,7 @@ void report_error(struct report *r, const char *fmt, ...)
         __attribute__((__format__(__printf__, 2, 3)));
 void report_init(struct report *r);
 void report_clear(struct report *r);
+const char *report_getmessage(const struct report *r);
 
 __END_EXTERN_C
 
