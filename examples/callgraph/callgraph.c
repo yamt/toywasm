cbd6c5c2e0d0f7745cf7c3c241aaf058362f420c
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-09 08:24:34 +0900

    examples/callgraph: use a prefix different from jansson

--- examples/callgraph/callgraph.c
+++ examples/callgraph/callgraph.c
@@ -26,7 +26,7 @@ dump_calls(const struct module *m, uint32_t i, struct nametable *table)
         const uint8_t *insn = func->e.start;
         json_t *a = json_array();
         if (a == NULL) {
-                json_fatal();
+                jsonutil_fatal();
         }
         struct parse_expr_context ctx;
         parse_expr_context_init(&ctx);
@@ -49,9 +49,9 @@ dump_calls(const struct module *m, uint32_t i, struct nametable *table)
                 case 0x10: /* call */
                         imm = &insn[1];
                         callee = read_leb_u32_nocheck(&imm);
-                        json_pack_and_append(a, "{sisbsi}", "pc", pc,
-                                             "tailcall", tailcall, "callee",
-                                             callee);
+                        jsonutil_pack_and_append(a, "{sisbsi}", "pc", pc,
+                                                 "tailcall", tailcall,
+                                                 "callee", callee);
                         break;
 #if defined(TOYWASM_ENABLE_WASM_TAILCALL)
                 case 0x13: /* return_call_indirect */
@@ -67,9 +67,9 @@ dump_calls(const struct module *m, uint32_t i, struct nametable *table)
                         if (ret != 0) {
                                 fatal(ret);
                         }
-                        json_pack_and_append(a, "{sisbsiss}", "pc", pc,
-                                             "tailcall", tailcall, "table",
-                                             tableidx, "type", typestr);
+                        jsonutil_pack_and_append(a, "{sisbsiss}", "pc", pc,
+                                                 "tailcall", tailcall, "table",
+                                                 tableidx, "type", typestr);
                         functype_string_free(typestr);
                         break;
                 default:
@@ -91,12 +91,12 @@ callgraph(const struct module *m)
         uint32_t i;
         json_t *j = json_object();
         if (j == NULL) {
-                json_fatal();
+                jsonutil_fatal();
         }
         json_t *a;
         struct nametable table;
         nametable_init(&table);
-        a = json_object_set_array(j, "funcs");
+        a = jsonutil_object_set_array(j, "funcs");
         for (i = 0; i < m->nimportedfuncs + m->nfuncs; i++) {
                 bool imported = i < m->nimportedfuncs;
                 struct name func_name;
@@ -116,11 +116,11 @@ callgraph(const struct module *m)
                         expr_size = json_integer(func->e.end - func->e.start);
 #endif
                 }
-                json_pack_and_append(a, "{ss#sisssbso*so*}", "name",
-                                     func_name.data, (int)func_name.nbytes,
-                                     "idx", i, "type", typestr, "imported",
-                                     (int)imported, "calls", calls,
-                                     "expr_size", expr_size);
+                jsonutil_pack_and_append(a, "{ss#sisssbso*so*}", "name",
+                                         func_name.data, (int)func_name.nbytes,
+                                         "idx", i, "type", typestr, "imported",
+                                         (int)imported, "calls", calls,
+                                         "expr_size", expr_size);
                 functype_string_free(typestr);
         }
         /*
@@ -129,7 +129,7 @@ callgraph(const struct module *m)
          * although it's limited, it should be enough to
          * cover the most of common cases.
          */
-        a = json_object_set_array(j, "elements");
+        a = jsonutil_object_set_array(j, "elements");
         for (i = 0; i < m->nelems; i++) {
                 const struct element *e = &m->elems[i];
                 if (e->mode != ELEM_MODE_ACTIVE) {
@@ -142,33 +142,33 @@ callgraph(const struct module *m)
                 for (j = 0; j < e->init_size; j++) {
                         uint32_t funcidx = e->funcs[j];
                         uint32_t tableidx = e->table;
-                        json_pack_and_append(a, "{sisi}", "tableidx", tableidx,
-                                             "funcidx", funcidx);
+                        jsonutil_pack_and_append(a, "{sisi}", "tableidx",
+                                                 tableidx, "funcidx", funcidx);
                 }
         }
-        a = json_object_set_array(j, "imports");
+        a = jsonutil_object_set_array(j, "imports");
         for (i = 0; i < m->nimports; i++) {
                 const struct import *im = &m->imports[i];
                 if (im->desc.type != EXTERNTYPE_FUNC) {
                         continue;
                 }
-                json_pack_and_append(
+                jsonutil_pack_and_append(
                         a, "{ss#ss#si}", "module_name", im->module_name.data,
                         (int)im->module_name.nbytes, "name", im->name.data,
                         (int)im->name.nbytes, "idx", i);
         }
-        a = json_object_set_array(j, "exports");
+        a = jsonutil_object_set_array(j, "exports");
         for (i = 0; i < m->nexports; i++) {
                 const struct wasm_export *ex = &m->exports[i];
                 if (ex->desc.type != EXTERNTYPE_FUNC) {
                         continue;
                 }
-                json_pack_and_append(a, "{ss#si}", "name", ex->name.data,
-                                     (int)ex->name.nbytes, "idx",
-                                     ex->desc.idx);
+                jsonutil_pack_and_append(a, "{ss#si}", "name", ex->name.data,
+                                         (int)ex->name.nbytes, "idx",
+                                         ex->desc.idx);
         }
         if (m->has_start) {
-                json_object_set_u32(j, "start", m->start);
+                jsonutil_object_set_u32(j, "start", m->start);
         }
         nametable_clear(&table);
         json_dumpf(j, stdout, JSON_INDENT(4));
