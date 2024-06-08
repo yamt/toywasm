0ccb6fe36ae64c9120dc6128c1af57651eab87fe
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-09 08:24:34 +0900

    examples/callgraph: use a prefix different from jansson

--- examples/callgraph/jsonutil.c
+++ examples/callgraph/jsonutil.c
@@ -4,14 +4,14 @@
 #include "jsonutil.h"
 
 void
-json_fatal(void)
+jsonutil_fatal(void)
 {
         fprintf(stderr, "json error\n");
         exit(1);
 }
 
 static void
-json_fatal_error(const json_error_t *e)
+jsonutil_fatal_error(const json_error_t *e)
 {
         fprintf(stderr,
                 "json error\ntext: %s\nsource: %s\nline: %d\ncolumn: "
@@ -21,19 +21,19 @@ json_fatal_error(const json_error_t *e)
 }
 
 void
-json_object_set_u32(json_t *o, const char *key, uint32_t u32)
+jsonutil_object_set_u32(json_t *o, const char *key, uint32_t u32)
 {
         json_t *v = json_integer(u32);
         if (v == NULL) {
-                json_fatal();
+                jsonutil_fatal();
         }
         if (json_object_set_new(o, key, v)) {
-                json_fatal();
+                jsonutil_fatal();
         }
 }
 
 void
-json_pack_and_append(json_t *a, const char *fmt, ...)
+jsonutil_pack_and_append(json_t *a, const char *fmt, ...)
 {
         json_error_t error;
         va_list ap;
@@ -41,22 +41,22 @@ json_pack_and_append(json_t *a, const char *fmt, ...)
         json_t *e = json_vpack_ex(&error, 0, fmt, ap);
         va_end(ap);
         if (e == NULL) {
-                json_fatal_error(&error);
+                jsonutil_fatal_error(&error);
         }
         if (json_array_append_new(a, e)) {
-                json_fatal();
+                jsonutil_fatal();
         }
 }
 
 json_t *
-json_object_set_array(json_t *o, const char *key)
+jsonutil_object_set_array(json_t *o, const char *key)
 {
         json_t *a = json_array();
         if (a == NULL) {
-                json_fatal();
+                jsonutil_fatal();
         }
         if (json_object_set_new(o, key, a)) {
-                json_fatal();
+                jsonutil_fatal();
         }
         return a;
 }
