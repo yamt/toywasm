3ae70507fd9b8ac5fd20d46d7db0015846237c20
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-15 17:40:09 +0900

    examples/fuzz: adapt to mem_context

--- examples/fuzz/main.cxx
+++ examples/fuzz/main.cxx
@@ -3,16 +3,19 @@
 #include <toywasm/exec_context.h>
 #include <toywasm/instance.h>
 #include <toywasm/load_context.h>
+#include <toywasm/mem.h>
 #include <toywasm/module.h>
 #include <toywasm/report.h>
 
 extern "C" int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
+        struct mem_context mctx;
+        mem_context_init(&mctx);
         struct module *m;
         int ret;
         struct load_context ctx;
-        load_context_init(&ctx);
+        load_context_init(&ctx, &mctx);
         ret = module_create(&m, data, data + size, &ctx);
         load_context_clear(&ctx);
         if (ret != 0) {
@@ -21,13 +24,13 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         struct instance *inst;
         struct report report;
         report_init(&report);
-        ret = instance_create_no_init(m, &inst, NULL, &report);
+        ret = instance_create_no_init(&mctx, m, &inst, NULL, &report);
         report_clear(&report);
         if (ret != 0) {
                 goto fail_instantiate;
         }
         struct exec_context ectx;
-        exec_context_init(&ectx, inst);
+        exec_context_init(&ectx, inst, &mctx);
         /* avoid infinite loops in the start function */
         const static atomic_uint one = 1;
         ectx.intrp = &one;
@@ -36,7 +39,8 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         exec_context_clear(&ectx);
         instance_destroy(inst);
 fail_instantiate:
-        module_destroy(m);
+        module_destroy(&mctx, m);
 fail_load:
+        mem_context_clear(&mctx);
         return 0;
 }
