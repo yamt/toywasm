6682afd72528b5f44a47ce8c63fdc58b8133efd1
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-15 17:39:38 +0900

    mem.h: extern C

--- lib/mem.h
+++ lib/mem.h
@@ -8,6 +8,8 @@ struct mem_context {
         struct mem_context *parent;
 };
 
+__BEGIN_EXTERN_C
+
 void mem_context_init(struct mem_context *ctx);
 void mem_context_clear(struct mem_context *ctx);
 void *__must_check mem_alloc(struct mem_context *ctx, size_t sz) __malloc_like
@@ -18,3 +20,5 @@ void *__must_check mem_calloc(struct mem_context *ctx, size_t a, size_t b);
 void mem_free(struct mem_context *ctx, void *p, size_t sz);
 void *__must_check mem_resize(struct mem_context *ctx, void *p, size_t oldsz,
                               size_t newsz) __malloc_like __alloc_size(4);
+
+__END_EXTERN_C
