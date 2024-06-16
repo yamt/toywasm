339f5d65776380b359c3121b47a2a43aae8723ed
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-16 13:47:39 +0900

    str_to_uint.c: add str_to_size

--- cli/str_to_uint.h
+++ cli/str_to_uint.h
@@ -1,4 +1,6 @@
+#include <stddef.h>
 #include <stdint.h>
 
 int str_to_uint(const char *s, int base, uintmax_t *resultp);
 int str_to_u32(const char *s, int base, uint32_t *resultp);
+int str_to_size(const char *s, int base, size_t *resultp);
