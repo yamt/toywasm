821d6903bd1347b0596aefb90df59093098c228b
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-06-16 22:47:01 +0900

    examples/fuzz/run.sh: pass through the arguments

--- examples/fuzz/run.sh
+++ examples/fuzz/run.sh
@@ -2,4 +2,4 @@
 
 export ASAN_OPTIONS=detect_leaks=1
 mkdir -p corpus
-./build/build-app/fuzz-instantiate -rss_limit_mb=1000000 -malloc_limit_mb=100000 corpus
+./build/build-app/fuzz-instantiate -rss_limit_mb=1000000 -malloc_limit_mb=100000 corpus "$@"
