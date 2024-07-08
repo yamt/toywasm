47076bd6dbf93e57f64f84b781ec83653766f271
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-07-08 20:30:24 +0900

    examples/fuzz/run.sh: bump malloc limit
    so that it can run rust test cases in wasi-testsuite repo.
    some of them are about ~3MB.

--- examples/fuzz/run.sh
+++ examples/fuzz/run.sh
@@ -2,4 +2,4 @@
 
 export ASAN_OPTIONS=detect_leaks=1
 mkdir -p corpus
-./build/build-app/fuzz-instantiate -rss_limit_mb=4000 -malloc_limit_mb=2 corpus "$@"
+./build/build-app/fuzz-instantiate -rss_limit_mb=4000 -malloc_limit_mb=3 corpus "$@"
