yamt/toywasm

commit 217a733089efc18f2a75193552df74de1db0a8ed

cli: use separate mem contexts for modules and instances

cli/main.c
@@ -296,6 +296,8 @@ int
 main(int argc, char *const *argv)
 {
         struct mem_context mctx0, *mctx = &mctx0;
+        struct mem_context modules_mctx0, *modules_mctx = &modules_mctx0;
+        struct mem_context instances_mctx0, *instances_mctx = &instances_mctx0;
         struct mem_context wasi_mctx0, *wasi_mctx = &wasi_mctx0;
         struct mem_context dyld_mctx0, *dyld_mctx = &dyld_mctx0;
         struct mem_context impobj_mctx0, *impobj_mctx = &impobj_mctx0;
@@ -334,9 +336,13 @@ main(int argc, char *const *argv)
 #endif
 #endif
         mem_context_init(mctx);
+        mem_context_init(modules_mctx);
+        mem_context_init(instances_mctx);
         mem_context_init(wasi_mctx);
         mem_context_init(dyld_mctx);
         mem_context_init(impobj_mctx);
+        modules_mctx->parent = mctx;
+        modules_mctx->parent = mctx;
         wasi_mctx->parent = mctx;
         dyld_mctx->parent = mctx;
         impobj_mctx->parent = mctx;
@@ -347,6 +353,8 @@ main(int argc, char *const *argv)
         }
         toywasm_repl_state_init(state);
         state->mctx = mctx;
+        state->modules_mctx = modules_mctx;
+        state->instances_mctx = instances_mctx;
         state->wasi_mctx = wasi_mctx;
         state->dyld_mctx = dyld_mctx;
         state->impobj_mctx = impobj_mctx;
@@ -616,6 +624,8 @@ fail:
         free(state);
         mem_context_clear(dyld_mctx);
         mem_context_clear(wasi_mctx);
+        mem_context_clear(instances_mctx);
+        mem_context_clear(modules_mctx);
         mem_context_clear(mctx);
         exit(exit_status);
 success:

cli/repl.c
@@ -205,6 +205,8 @@ toywasm_repl_reset(struct repl_state *state)
                 nbio_printf("=== memory consumption immediately before a repl "
                             "reset ===\n");
                 print_memory_usage(state->mctx, "total");
+                print_memory_usage(state->modules_mctx, "modules");
+                print_memory_usage(state->instances_mctx, "instances");
                 print_memory_usage(state->wasi_mctx, "wasi");
                 print_memory_usage(state->dyld_mctx, "dyld");
                 print_memory_usage(state->impobj_mctx, "impobj");
@@ -598,8 +600,8 @@ repl_load_from_buf(struct repl_state *state, const char *modname,
         mod->instance_mctx = mctx2 + 1;
         mem_context_init(mod->module_mctx);
         mem_context_init(mod->instance_mctx);
-        mod->module_mctx->parent = state->mctx;
-        mod->instance_mctx->parent = state->mctx;
+        mod->module_mctx->parent = state->modules_mctx;
+        mod->instance_mctx->parent = state->instances_mctx;
         struct load_context ctx;
         load_context_init(&ctx, mod->module_mctx);
         ctx.options = state->opts.load_options;

cli/repl.h
@@ -66,6 +66,8 @@ struct repl_state {
         struct timespec abstimeout;
         bool has_timeout;
         struct mem_context *mctx;
+        struct mem_context *modules_mctx;
+        struct mem_context *instances_mctx;
         struct mem_context *wasi_mctx;
         struct mem_context *dyld_mctx;
         struct mem_context *impobj_mctx;

commit af7c765dfcc26fb8aa6954271a8ee096be93d333

cli: fix --max-memory regression
the regression was introduced by
commit 217a733089efc18f2a75193552df74de1db0a8ed
("cli: use separate mem contexts for modules and instances")

cli/main.c
@@ -342,7 +342,7 @@ main(int argc, char *const *argv)
         mem_context_init(dyld_mctx);
         mem_context_init(impobj_mctx);
         modules_mctx->parent = mctx;
-        modules_mctx->parent = mctx;
+        instances_mctx->parent = mctx;
         wasi_mctx->parent = mctx;
         dyld_mctx->parent = mctx;
         impobj_mctx->parent = mctx;