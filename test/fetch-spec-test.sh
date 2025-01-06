a5cf6bda2b2e022775d4d2d54aa11afc2e1295c2
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-01-06 19:48:39 +0900

    test/fetch-spec-test.sh: update wasm-spec-test

--- test/fetch-spec-test.sh
+++ test/fetch-spec-test.sh
@@ -6,7 +6,7 @@ TEST_DIR=${TEST_DIR:-.wasm-spec-test}
 fetch_spec_json()
 {
     REPO=https://github.com/yamt/wasm-spec-test
-    REF=64654520d08b3fa968a50f5b1fc0e5e2c930feef
+    REF=c540d60742489d00543af137f744debfb9c76ecd
     mkdir "${TEST_DIR}"
     git -C "${TEST_DIR}" init
     git -C "${TEST_DIR}" fetch --depth 1 ${REPO} ${REF}
