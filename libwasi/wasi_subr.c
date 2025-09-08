34939862b202d087b7370bc3fb8697228cbfa4fc
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-09-08 20:46:15 +0900

    libwasi: simplify a bit

--- libwasi/wasi_subr.c
+++ libwasi/wasi_subr.c
@@ -117,12 +117,10 @@ fail:
 int
 wasi_validate_fstflags(uint32_t fstflags)
 {
-        if ((fstflags & WASI_FSTFLAG_ATIM_NOW) != 0 &&
-            (fstflags & WASI_FSTFLAG_ATIM) != 0) {
+        if ((~fstflags & (WASI_FSTFLAG_ATIM_NOW | WASI_FSTFLAG_ATIM)) == 0) {
                 return EINVAL;
         }
-        if ((fstflags & WASI_FSTFLAG_MTIM_NOW) != 0 &&
-            (fstflags & WASI_FSTFLAG_MTIM) != 0) {
+        if ((~fstflags & (WASI_FSTFLAG_MTIM_NOW | WASI_FSTFLAG_MTIM)) == 0) {
                 return EINVAL;
         }
         return 0;
