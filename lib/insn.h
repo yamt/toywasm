85ad7dedbe03860fe2a5f051f8c5b34ebd469f0b
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-10-04 18:24:31 +0900

    make insn.c a bit more self contained
    unexport instruction tables

--- lib/insn.h
+++ lib/insn.h
@@ -1,43 +1,4 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "toywasm_config.h"
-
-struct context;
-struct exec_context;
-struct validation_context;
-struct cell;
-
-struct exec_instruction_desc {
-        /*
-         * fetch_exec is called after fetching the first byte of
-         * the instrution. '*p' points to the second byte.
-         * it fetches and decodes the rest of the instrution,
-         * and then executes it.
-         */
-        int (*fetch_exec)(const uint8_t *p, struct cell *stack,
-                          struct exec_context *ctx);
-};
-
-struct instruction_desc {
-        const char *name;
-        int (*process)(const uint8_t **pp, const uint8_t *ep,
-                       struct context *ctx);
-#if defined(TOYWASM_USE_SEPARATE_VALIDATE)
-        int (*validate)(const uint8_t *p, const uint8_t *ep,
-                        struct validation_context *vctx);
-#endif
-        const struct instruction_desc *next_table;
-        unsigned int next_table_size;
-        unsigned int flags;
-};
-
-#define INSN_FLAG_CONST 1
-#if defined(TOYWASM_ENABLE_WASM_EXTENDED_CONST)
-#define INSN_FLAG_EXTENDED_CONST INSN_FLAG_CONST
-#else
-#define INSN_FLAG_EXTENDED_CONST 0
-#endif
-
-extern const struct instruction_desc instructions[];
-extern const size_t instructions_size;
+uint32_t read_insn(const uint8_t **pp);
