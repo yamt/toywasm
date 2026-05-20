1484532235e95b1fe80d7f561da17dbe8558177d
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-05-20 23:15:19 +0900

    build-wasm32-wasi.sh: make it easier to build for wasi-threads
    eg.
    BUILD_WASM32_WASI_BUILD_FOR_WASI_THREADS=1 ./build-wasm32-wasi.sh

--- build-wasm32-wasi.sh
+++ build-wasm32-wasi.sh
@@ -11,9 +11,13 @@ set -e
 MAJOR=${WASI_SDK_MAJOR:-33}
 MINOR=${WASI_SDK_MINOR:-0}
 WASI_SDK_DIR=${WASI_SDK_DIR:-$(pwd)/.wasi-sdk-${MAJOR}.${MINOR}}
-CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-${WASI_SDK_DIR}/share/cmake/wasi-sdk-p1.cmake}
-if [ ! -e ${WASI_SDK_DIR}/share/cmake/wasi-sdk-p1.cmake ]; then
-    CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-${WASI_SDK_DIR}/share/cmake/wasi-sdk.cmake}
+if [ -n "${BUILD_WASM32_WASI_BUILD_FOR_WASI_THREADS}" ]; then
+    CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-${WASI_SDK_DIR}/share/cmake/wasi-sdk-pthread.cmake}
+else
+    CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-${WASI_SDK_DIR}/share/cmake/wasi-sdk-p1.cmake}
+    if [ ! -e ${WASI_SDK_DIR}/share/cmake/wasi-sdk-p1.cmake ]; then
+        CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-${WASI_SDK_DIR}/share/cmake/wasi-sdk.cmake}
+    fi
 fi
 DIST_DIR=.dist
 
